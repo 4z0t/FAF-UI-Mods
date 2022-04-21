07282569694f5866db8adfc4ebf08744945e5a1b
4z0t <gastec139@gmail.com>
2022-04-21 13:26:06 +0300

    Add STP

---
+++ mods/STP/hook/lua/keymap/misckeyactions.lua
@@ -0,0 +1,14 @@
+local KeyMapper = import('/lua/keymap/keymapper.lua')
+
+function SetWeaponPrioritiesSpecific()
+    local info = GetRolloverInfo()
+    if info and info.blueprintId ~= 'unknown' then
+        -- local bp = __blueprints[info.blueprintId].Description
+        -- LOG(repr(bp))
+        -- LOG(info.blueprintId)
+        SetWeaponPriorities('{categories.'..info.blueprintId..'}', LOC(__blueprints[info.blueprintId].Description), false)
+    end
+end
+--Specific
+KeyMapper.SetUserKeyAction('target_specific', {action = 'UI_Lua import("/lua/keymap/misckeyactions.lua").SetWeaponPrioritiesSpecific()', category = 'Target priorities', order = 109})
+KeyMapper.SetUserKeyAction('Shift_target_specific', {action = 'UI_Lua import("/lua/keymap/misckeyactions.lua").SetWeaponPrioritiesSpecific()', category = 'Target priorities', order = 110})
