20b4b54b4cb76bdb55f535ed266f8860d5d764cb
4z0t <gastec139@gmail.com>
2022-04-21 14:15:49 +0300

    KBO: swap lines

--- mods/KBO/modules/view.lua
+++ mods/KBO/modules/view.lua
@@ -13,7 +13,6 @@ local CheckBox = import('/lua/maui/checkbox.lua').Checkbox
 local UIMain = import('/lua/ui/uimain.lua')
 local From = import('/mods/UMT/modules/linq.lua').From
 
-
 local GUI
 function init()
     if IsDestroyed(GUI) then
@@ -78,11 +77,8 @@ function CreateUI(parent)
 
     end
 
-
-
-
     local strLen = string.len
-    
+
     local legalCategories = From({'BUILTBYTIER1FACTORY', 'BUILTBYTIER2FACTORY', 'BUILTBYTIER3FACTORY',
                                   'BUILTBYTIER1ENGINEER', 'BUILTBYTIER2ENGINEER', 'BUILTBYTIER3ENGINEER',
                                   'BUILTBYCOMMANDER', 'BUILTBYQUANTUMGATE', 'BUILTBYLANDTIER3FACTORY', -- special for sparky
@@ -113,88 +109,89 @@ function CreateUI(parent)
     group.construction = ConstructionScrollArea(group, constructionBPs, 5)
     LayoutHelpers.AtLeftTopIn(group.construction, group, 100, 100)
     group.categories = {}
-    -- From(divisions):Foreach(function(i, div)
-    --     if i == 1 then
-    --         return
-    --     end
-    --     local name = UIUtil.CreateText(group, div.name, 14, UIUtil.bodyFont, true)
-    --     group.categories[div.name] = {}
-    --     group.categories[div.name].selectors = {}
-    --     LayoutHelpers.AtLeftTopIn(name, group, 100, i * 80)
-    --     From(skins):Foreach(function(k, skin)
 
-    --         local upperSkin = string.upper(skin)
-    --         local selector = BlueprintItem(group, skin)
-    --         selector.bps = bps:Where(function(id, bp)
-    --             local categories = From(bp.Categories)
-    --             return categories:Contains(upperSkin) and From(div.all):All(function(_, cat)
-    --                 return categories:Contains(cat)
-    --             end) and From(div.any):Any(function(_, cat)
-    --                 return categories:Contains(cat)
-    --             end)
-    --         end):Keys():Sort(function(a, b)
-    --             return string.sub(a, 4) < string.sub(b, 4)
-    --         end)
-    --         LayoutHelpers.AtLeftTopIn(selector, group, 100 + 200 * (k - 1), i * 80 + 20)
-    --         selector.OnClick = function(self, modifiers, bluprint)
-    --             if modifiers.Left then
-    --                 -- if IsDestroyed(self.menu) then
-    --                 local menu = BlueprintSelector(self, self.bps:ToDictionary(), skin)
-    --                 LayoutHelpers.Below(menu, self)
-    --                 menu.OnItemClick = function(control, bluprint)
-    --                     self:SetBlueprint(bluprint)
-    --                     control:Destroy()
-    --                 end
-    --                 self.menu = menu
-    --                 -- end
-    --             elseif modifiers.Right then
-    --                 self:SetBlueprint()
-    --             end
-    --         end
-    --         group.categories[div.name].selectors[skin] = selector
+    From(divisions):Foreach(function(i, div)
+        if i == 1 then
+            return
+        end
+        local name = UIUtil.CreateText(group, div.name, 14, UIUtil.bodyFont, true)
+        group.categories[div.name] = {}
+        group.categories[div.name].selectors = {}
+        LayoutHelpers.AtLeftTopIn(name, group, 100, 200 + i * 80)
+        From(skins):Foreach(function(k, skin)
 
-    --     end)
-    --     local fillButton = UIUtil.CreateButtonStd(group, '/widgets02/small', 'fill', 16)
-    --     LayoutHelpers.AtLeftTopIn(fillButton, group, 900, i * 80 + 30)
-    --     LayoutHelpers.SetDimensions(fillButton, 100, 30)
-    --     LayoutHelpers.DepthOverParent(fillButton, group)
-    --     fillButton.OnClick = function(control, modifiers)
-    --         -- logic for filling alias blueprints
-    --         if modifiers.Left then
-    --             local bp
-    --             local skin
-    --             for selectorSkin, selector in group.categories[div.name].selectors do
-    --                 if selector:GetBlueprint() then
-    --                     if bp then
-    --                         return
-    --                     end
-    --                     bp = selector:GetBlueprint()
-    --                     skin = selectorSkin
-    --                 end
-    --             end
-    --             if bp then
-    --                 local suffix = string.sub(bp, 3)
-    --                 local pref = string.sub(bp, 1, 2)
-    --                 local prefixId = 1
-    --                 for id, prefix in prefixes[skin] do
-    --                     if pref == prefix then
-    --                         prefixId = id
-    --                         break
-    --                     end
-    --                 end
-    --                 for prefixSkin, prefix in prefixes do
-    --                     if group.categories[div.name].selectors[prefixSkin].bps:Contains(prefix[prefixId] .. suffix) then
-    --                         group.categories[div.name].selectors[prefixSkin]:SetBlueprint(prefix[prefixId] .. suffix)
-    --                     end
-    --                 end
-    --             end
-    --         elseif modifiers.Right then
-    --             for _, selector in group.categories[div.name].selectors do
-    --                 selector:SetBlueprint()
-    --             end
-    --         end
-    --     end
-    -- end)
+            local upperSkin = string.upper(skin)
+            local selector = BlueprintItem(group, skin)
+            selector.bps = bps:Where(function(id, bp)
+                local categories = From(bp.Categories)
+                return categories:Contains(upperSkin) and From(div.all):All(function(_, cat)
+                    return categories:Contains(cat)
+                end) and From(div.any):Any(function(_, cat)
+                    return categories:Contains(cat)
+                end)
+            end):Keys():Sort(function(a, b)
+                return string.sub(a, 4) < string.sub(b, 4)
+            end)
+            LayoutHelpers.AtLeftTopIn(selector, group, 100 + 200 * (k - 1), 200 + i * 80 + 20)
+            selector.OnClick = function(self, modifiers, bluprint)
+                if modifiers.Left then
+                    -- if IsDestroyed(self.menu) then
+                    local menu = BlueprintSelector(self, self.bps:ToDictionary(), skin)
+                    LayoutHelpers.Below(menu, self)
+                    menu.OnItemClick = function(control, bluprint)
+                        self:SetBlueprint(bluprint)
+                        control:Destroy()
+                    end
+                    self.menu = menu
+                    -- end
+                elseif modifiers.Right then
+                    self:SetBlueprint()
+                end
+            end
+            group.categories[div.name].selectors[skin] = selector
+
+        end)
+        local fillButton = UIUtil.CreateButtonStd(group, '/widgets02/small', 'fill', 16)
+        LayoutHelpers.AtLeftTopIn(fillButton, group, 900, 200 + i * 80 + 30)
+        LayoutHelpers.SetDimensions(fillButton, 100, 30)
+        LayoutHelpers.DepthOverParent(fillButton, group)
+        fillButton.OnClick = function(control, modifiers)
+            -- logic for filling alias blueprints
+            if modifiers.Left then
+                local bp
+                local skin
+                for selectorSkin, selector in group.categories[div.name].selectors do
+                    if selector:GetBlueprint() then
+                        if bp then
+                            return
+                        end
+                        bp = selector:GetBlueprint()
+                        skin = selectorSkin
+                    end
+                end
+                if bp then
+                    local suffix = string.sub(bp, 3)
+                    local pref = string.sub(bp, 1, 2)
+                    local prefixId = 1
+                    for id, prefix in prefixes[skin] do
+                        if pref == prefix then
+                            prefixId = id
+                            break
+                        end
+                    end
+                    for prefixSkin, prefix in prefixes do
+                        if group.categories[div.name].selectors[prefixSkin].bps:Contains(prefix[prefixId] .. suffix) then
+                            group.categories[div.name].selectors[prefixSkin]:SetBlueprint(prefix[prefixId] .. suffix)
+                        end
+                    end
+                end
+            elseif modifiers.Right then
+                for _, selector in group.categories[div.name].selectors do
+                    selector:SetBlueprint()
+                end
+            end
+        end
+    end)
 
     return group
 end
@@ -337,11 +334,14 @@ ConstructionScrollArea = Class(IScrollable) {
     __init = function(self, parent, blueprints, itemCount)
         IScrollable.__init(self, parent)
         LayoutHelpers.DepthOverParent(self, parent, 10)
+
         self._blueprints = blueprints
         self._topLine = 1
         self._data = {}
         self._numLines = DEFAULT_CONSTRUCTION_ITEM_COUNT
         self._dataSize = itemCount or DEFAULT_CONSTRUCTION_ITEM_COUNT
+        self._swapIndex = false
+
         self._title = UIUtil.CreateText(self, 'Construction', 14, UIUtil.bodyFont, true)
         LayoutHelpers.AtLeftTopIn(self._title, self, 2, 2)
         self:CreateItems()
@@ -372,11 +372,37 @@ ConstructionScrollArea = Class(IScrollable) {
 
     CreateItem = function(self, parent, index)
         local group = Group(parent)
-        LayoutHelpers.SetDimensions(group, 800, 100)
+        LayoutHelpers.SetDimensions(group, 800, 70)
         group.selectors = {}
-        From(skins):Foreach(function(k, skin)
+        group.id = index
 
-            local upperSkin = string.upper(skin)
+        group.indexText = UIUtil.CreateText(group, tostring(index), 20, UIUtil.titleFont, true)
+        LayoutHelpers.AtLeftTopIn(group.indexText, group, 4, 5)
+        group.indexText:SetColor(UIUtil.fontOverColor)
+
+        group.indexText.HandleEvent = function(control, event)
+            if event.Type == 'ButtonPress' then -- swap logic for lines
+                if self._swapIndex and self._swapIndex~=group.id  then
+                    local temp = self._data[group.id]
+                    self._data[group.id] = self._data[self._swapIndex]
+                    self._data[self._swapIndex] = temp
+                    self._swapIndex = false
+                    self:CalcVisible()
+                    return true
+                else
+                    self._swapIndex = group.id
+                end
+                return false
+            elseif event.Type == 'MouseExit' then
+                control:SetColor(UIUtil.fontOverColor)
+                return true
+            elseif event.Type == 'MouseEnter' then
+                control:SetColor(UIUtil.highlightColor)
+                return true
+            end
+        end
+
+        From(skins):Foreach(function(k, skin)
             local selector = BlueprintItem(group, skin)
             selector.bps = From(self._blueprints[skin])
             selector.id = index
@@ -403,7 +429,7 @@ ConstructionScrollArea = Class(IScrollable) {
 
         end)
         local fillButton = UIUtil.CreateButtonStd(group, '/widgets02/small', 'fill', 16)
-        LayoutHelpers.AtLeftTopIn(fillButton, group, 900, 50)
+        LayoutHelpers.AtLeftTopIn(fillButton, group, 900, 35)
         LayoutHelpers.SetDimensions(fillButton, 100, 30)
         LayoutHelpers.DepthOverParent(fillButton, group)
         fillButton.OnClick = function(control, modifiers)
@@ -449,6 +475,8 @@ ConstructionScrollArea = Class(IScrollable) {
     end,
 
     RenderLine = function(self, lineIndex, scrollIndex)
+        self._lineGroup._lines[lineIndex].indexText:SetText(tostring(scrollIndex))
+        self._lineGroup._lines[lineIndex].id = scrollIndex
         for skin, selector in self._lineGroup._lines[lineIndex].selectors do
             local bp
             if self._data[scrollIndex] then
