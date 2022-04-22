0e46d43d9a7eb21cb6f3feef51fac8b45ac9452e
4z0t <gastec139@gmail.com>
2022-04-22 18:10:09 +0300

    TIS: change dep mod id

--- mods/TIS/mod_info.lua
+++ mods/TIS/mod_info.lua
@@ -1,6 +1,6 @@
 name = "TeamInfo Share"
-uid = "teaminfo-share-v07"
-version = 7
+uid = "teaminfo-share-v08"
+version = 8
 copyright = ""
 description = [[Shares data in team about nukes, smds and exps completions
 requires Common mod tools and UI mod tools]]
@@ -11,8 +11,8 @@ selectable = true
 enabled = true
 exclusive = false
 ui_only = true
-requires = {"zcbf6277-24e3-437a-b968-Common-v1", "ui-mod-tools-4z0t-v01"}
+requires = {"zcbf6277-24e3-437a-b968-Common-v1", "ui-mod-tools-4z0t-v02"}
 requiresNames = {}
 conflicts = {}
-before = {"zcbf6277-24e3-437a-b968-Common-v1", "ui-mod-tools-4z0t-v01"}
+before = {"zcbf6277-24e3-437a-b968-Common-v1", "ui-mod-tools-4z0t-v02"}
 after = {}
