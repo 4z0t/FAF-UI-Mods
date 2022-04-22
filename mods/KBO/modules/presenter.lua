7940e6bf30670e75ecb6620ff568e0db78092110
4z0t <84857900+4z0t@users.noreply.github.com>
2022-04-22 12:42:35 +0300

    KBO: add presenter logic

---
+++ mods/KBO/modules/presenter.lua
@@ -0,0 +1,54 @@
+
+function init()
+
+end
+function AddConstructionBlueprints()
+
+end
+
+function SetConstructionBlueprint(index, faction, bp)
+
+end
+
+function SetBlueprint(category, faction, bp)
+
+end
+
+function Swap(i1,i2)
+
+end
+
+function FillBlueprints(category, bps)
+    local res = {}
+    ...
+    return res
+end
+
+function ClearBlueprints(category)
+
+end
+
+function FillConstructionBlueprints( index, bps)
+
+end
+
+function ClearConstructionBlueprints( index)
+
+end
+
+function FetchConstructionBlueprint( index, faction)
+
+end
+
+function FetchBlueprint(category, faction)
+
+end
+
+function FetchConstructionBlueprints(index, faction)
+
+end
+
+function FetchBlueprints(category)
+    
+end
+
