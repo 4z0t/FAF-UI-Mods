6791e9acc90f31eac4a47c59ae4d667035441f44
4z0t <gastec139@gmail.com>
2022-04-24 15:30:02 +0300

    HBO: implicit dep

--- mods/HBO/modules/main.lua
+++ mods/HBO/modules/main.lua
@@ -3,6 +3,15 @@ local Model = import('model.lua')
 local View = import("/mods/HBO/modules/views/view.lua")
 
 function init(isReplay)
-    Model.init()
-    Presenter.init()
+    if exists('/mods/UMT/modules/linq.lua') then
+        Model.init()
+        Presenter.init()
+    else
+        ForkThread(function()
+            WaitSeconds(4)
+            for i = 1,10 do
+                print("HotBuild Overhaul requires UI mod tools!!!")
+            end
+        end)
+    end
 end
