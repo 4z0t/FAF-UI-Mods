07b76387e99b6f13251e9fbb5a94ddefd02b4768
4z0t <gastec139@gmail.com>
2022-04-24 16:05:58 +0300

    HBO: Version 4

--- mods/HBO/mod_info.lua
+++ mods/HBO/mod_info.lua
@@ -1,6 +1,6 @@
 name = "HotBuild Overhaul"
-uid = "hotbuild-overhaul-4z0t-v03"
-version = 3
+uid = "hotbuild-overhaul-4z0t-v04"
+version = 4
 copyright = ""
 description = [[Make your own hotbuild keybinds!
 (you can find editor in F1 menu)
