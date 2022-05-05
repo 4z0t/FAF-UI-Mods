5da32c166c0f654f88286c2983b3ad614df63d4d
4z0t <gastec139@gmail.com>
2022-05-05 11:19:39 +0300

    UMT: importer

--- mods/UMT/modules/main.lua
+++ mods/UMT/modules/main.lua
@@ -1,6 +1,9 @@
 local From = import('linq.lua').from
 local Range = import('linq.lua').range
+import('Importer.lua')
 
 function init(isReplay)
     --import('tests.lua').main()
 end
+
+local Bitmap = require.lua.maui.bitmap().Bitmap
