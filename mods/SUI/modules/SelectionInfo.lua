9c15fa1e045d1735ee560018254f12f6e15d760f
4z0t <84857900+4z0t@users.noreply.github.com>
2022-05-09 22:18:21 +0300

    SUI: Selected Units Info

---
+++ mods/SUI/modules/SelectionInfo.lua
@@ -0,0 +1,136 @@
+local Group = import("/lua/maui/group.lua").Group
+local UIUtil = import("/lua/ui/uiutil.lua")
+local LayoutHelpers = import("/lua/maui/layouthelpers.lua")
+local Prefs = import('/lua/user/prefs.lua')
+local Dragger = import('/lua/maui/dragger.lua').Dragger
+local Bitmap = import('/lua/maui/bitmap.lua').Bitmap
+
+local VERTICAL_OFFSET = 10
+
+
+SelectionInfo = Class(Bitmap) {
+    __init = function(self, parent)
+        Bitmap.__init(self, parent)
+        LayoutHelpers.SetDimensions(self, 110, 60)
+        LayoutHelpers.DepthOverParent(self, parent, 500)
+        self:SetSolidColor('33000000')
+        local pos = self:_LoadPosition()
+        LayoutHelpers.AtLeftTopIn(self, parent, pos.left , VERTICAL_OFFSET)
+
+        self._massCost = UIUtil.CreateText(self, "0", 14, UIUtil.bodyFont, true)
+        self._massCost:SetColor("FFB8F400")
+        LayoutHelpers.AtRightTopIn(self._massCost, self,2,2)
+        self._massCost:DisableHitTest(true)
+    
+        self._energyCost = UIUtil.CreateText(self, "0", 14, UIUtil.bodyFont, true)
+        self._energyCost:SetColor("FFF8C000")
+        LayoutHelpers.AnchorToBottom(self._energyCost, self._massCost, 2)
+        LayoutHelpers.AtRightIn(self._energyCost, self._massCost)
+        self._energyCost:DisableHitTest(true)
+    
+        self._massRate = UIUtil.CreateText(self, "0", 14, UIUtil.bodyFont, true)
+        self._massRate:SetColor("FFB8F400")
+        LayoutHelpers.AtLeftTopIn(self._massRate, self,2,2)
+        self._massRate:DisableHitTest(true)
+    
+        self._energyRate = UIUtil.CreateText(self, "0", 14, UIUtil.bodyFont, true)
+        self._energyRate:SetColor("FFF8C000")
+        LayoutHelpers.Below(self._energyRate,  self._massRate, 2)
+        self._energyRate:DisableHitTest(true)
+    
+        self._buildRate = UIUtil.CreateText(self, "", 14, UIUtil.bodyFont, true)
+        self._buildRate:SetColor("FFFFFF00")
+        LayoutHelpers.Below(self._buildRate, self._energyRate, 2)
+        self._buildRate:DisableHitTest(true)
+
+    end,
+
+    HandleEvent = function(self, event)
+        if event.Type == 'ButtonPress' and event.Modifiers.Middle then
+            local drag = Dragger()
+            local offX = event.MouseX - self.Left()
+            drag.OnMove = function(dragself, x, y)
+                self.Left:Set(x - offX)
+                GetCursor():SetTexture(UIUtil.GetCursor('MOVE_WINDOW'))
+            end
+            drag.OnRelease = function(dragself)
+                self:_SavePosition()
+                GetCursor():Reset()
+                drag:Destroy()
+            end
+            PostDragger(self:GetRootFrame(), event.KeyCode, drag)
+            return true
+        end
+        return false
+    end,
+
+    Update = function(self, units)
+        if table.empty(units) then
+            self:Hide()
+        else
+            self:Show()
+            local massRate = 0
+            local energyRate = 0
+            local massCost = 0
+            local energyCost = 0
+            local totalbr = 0
+    
+            for index, unit in units do
+                if not unit:IsDead() then
+                    local econData = unit:GetEconData()
+    
+                    massRate = massRate - econData["massRequested"] + econData["massProduced"]
+                    energyRate = energyRate - econData["energyRequested"] + econData["energyProduced"]
+    
+                    local br = 0
+                    if unit:IsInCategory("ENGINEER") or unit:IsInCategory("FACTORY") then
+                        br = unit:GetBlueprint().Economy.BuildRate
+                    end
+                    if unit:IsInCategory("COMMAND") then
+                        br = unit:GetBuildRate()
+                    end
+                    totalbr = totalbr + br
+                    if not unit:IsInCategory("COMMAND") then
+                        massCost = massCost + unit:GetBlueprint().Economy.BuildCostMass
+                        energyCost = energyCost + unit:GetBlueprint().Economy.BuildCostEnergy
+                    end
+                end
+            end
+            self._massCost:SetText(string.format("%d", massCost))
+            self._energyCost:SetText(string.format("%d", energyCost))
+    
+            if massRate < 0 then
+                self._massRate:SetText(string.format("%d", massRate))
+                self._massRate:SetColor("fff30017")
+            else
+                self._massRate:SetText(string.format("+%d", massRate))
+                self._massRate:SetColor("FFB8F400")
+            end
+    
+            if energyRate < 0 then
+                self._energyRate:SetText(string.format("%d", energyRate))
+                self._energyRate:SetColor("fff30017")
+            else
+                self._energyRate:SetText(string.format("+%d", energyRate))
+                self._energyRate:SetColor("FFF8C000")
+            end
+    
+            if totalbr ~= 0 then
+                self._buildRate:SetText(string.format("+%d", totalbr))
+            else
+                self._buildRate:Hide()
+            end
+        end
+    end,
+    _LoadPosition = function(self)
+        return Prefs.GetFromCurrentProfile('SUIpos') or {
+            left = 500,
+        }
+    end,
+
+    _SavePosition = function(self)
+        Prefs.SetToCurrentProfile("SUIpos", {
+            left = self.Left(),
+        })
+    end
+}
