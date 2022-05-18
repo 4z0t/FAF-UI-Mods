e1bd8c2f234f253bc57b400cc7ea3addb1004762
4z0t <gastec139@gmail.com>
2022-05-18 15:49:56 +0300

    UMT: OptionVar

---
+++ mods/UMT/modules/OptionVar.lua
@@ -0,0 +1,50 @@
+local LazyVar = import("/lua/lazyvar.lua")
+local Prefs = import("/lua/user/prefs.lua")
+
+local OptionVarMetaTable = {}
+OptionVarMetaTable.__index = OptionVarMetaTable
+
+function OptionVarMetaTable:Get()
+    return self._lv()
+end
+
+function OptionVarMetaTable:Set(value)
+    self._prev = self._lv()
+    self._lv:Set(value)
+    self:OnChange()
+end
+
+function OptionVarMetaTable:Reset()
+    if self._prev ~= nil then
+        self:Set(self._prev)
+        self._prev = nil
+    end
+end
+
+function OptionVarMetaTable:Save()
+    Prefs.SetToCurrentProfile(self._m, table.merged(Prefs.GetFromCurrentProfile(self._m), {
+        [self._o] = self._lv()
+    }))
+    self._prev = nil
+end
+
+function Create(modOptionName, subOption, default)
+
+    local val = Prefs.GetFromCurrentProfile(modOptionName) and Prefs.GetFromCurrentProfile(modOptionName)[subOption]
+    if val == nil then
+        Prefs.SetToCurrentProfile(modOptionName, table.merged(Prefs.GetFromCurrentProfile(modOptionName) or {}, {
+            [subOption] = default
+        }))
+    end
+    
+    local result = {
+        _m = modOptionName,
+        _o = subOption,
+        _lv = LazyVar.Create(val or default),
+        _prev = nil,
+        OnChange = function(self)
+        end
+    }
+    setmetatable(result, OptionVarMetaTable)
+    return result
+end
