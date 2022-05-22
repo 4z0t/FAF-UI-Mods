5c7f02ee08fcd0da8c5b88c89df84314e9619064
4z0t <gastec139@gmail.com>
2022-05-22 19:14:33 +0300

    EUT: add implicit dep check

--- mods/EUT/modules/main.lua
+++ mods/EUT/modules/main.lua
@@ -1,7 +1,14 @@
-local MexManager = import('mexmanager.lua')
-local MexPanel = import('mexpanel.lua')
-
 function main()
-    MexManager.init()
-    MexPanel.init()
+    if not exists("/mods/UMT/mod_info.lua") or not exists("/mods/common/units.lua") then
+        ForkThread(function()
+            WaitSeconds(4)
+            print("ECO UI tools requires UI mod tools and Common Mod tools!!!")
+        end)
+        return
+    else
+        local MexManager = import("mexmanager.lua")
+        local MexPanel = import("mexpanel.lua")
+        MexManager.init()
+        MexPanel.init()
+    end
 end
