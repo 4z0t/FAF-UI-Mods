d2b01a0ef0b5154f28e5220719e0205647c05670
4z0t <gastec139@gmail.com>
2022-05-29 16:26:56 +0300

    EUT: version check for UMT

--- mods/EUT/modules/main.lua
+++ mods/EUT/modules/main.lua
@@ -1,16 +1,16 @@
 function main()
-    if not exists("/mods/UMT/mod_info.lua") or not exists("/mods/common/units.lua") then
-        ForkThread(function()
-            WaitSeconds(4)
-            print("ECO UI tools requires UI mod tools and Common Mod tools!!!")
-        end)
-        return
-    else
+    if exists("/mods/UMT/mod_info.lua") and import("/mods/UMT/mod_info.lua").version >= 4 then
         local MexManager = import("mexmanager.lua")
         local MexPanel = import("mexpanel.lua")
         local MexOverlay = import("mexoverlay.lua")
         MexManager.init()
         MexPanel.init()
         MexOverlay.init()
+    else
+        ForkThread(function()
+            WaitSeconds(4)
+            print("ECO UI tools requires UI mod tools version 4 and higher!!!")
+        end)
+        return
     end
 end
