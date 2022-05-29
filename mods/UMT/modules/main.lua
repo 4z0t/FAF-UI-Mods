295dba06d615b27477c8e6c94f68cc592b586fef
4z0t <gastec139@gmail.com>
2022-05-29 16:14:14 +0300

    UMT: adjust importer logic

--- mods/UMT/modules/main.lua
+++ mods/UMT/modules/main.lua
@@ -3,7 +3,5 @@ local Range = import('linq.lua').range
 import('Importer.lua')
 
 function init(isReplay)
-    --import('tests.lua').main()
+    import('tests.lua').main()
 end
-
-local Bitmap = require.lua.maui.bitmap().Bitmap
