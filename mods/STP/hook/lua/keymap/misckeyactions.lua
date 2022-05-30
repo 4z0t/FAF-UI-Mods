56536a03e9721938656c3000fd91e632f5875753
4z0t <gastec139@gmail.com>
2022-05-30 12:43:31 +0300

    STP: proper unit names

--- mods/STP/hook/lua/keymap/misckeyactions.lua
+++ mods/STP/hook/lua/keymap/misckeyactions.lua
@@ -1,23 +1,29 @@
-local KeyMapper = import('/lua/keymap/keymapper.lua')
+local KeyMapper = import("/lua/keymap/keymapper.lua")
 
 function SetWeaponPrioritiesSpecific()
     local info = GetRolloverInfo()
-    if info and info.blueprintId ~= 'unknown' then
-        -- local bp = __blueprints[info.blueprintId].Description
-        -- LOG(repr(bp))
-        -- LOG(info.blueprintId)
-        SetWeaponPriorities('{categories.' .. info.blueprintId .. '}', LOC(__blueprints[info.blueprintId].Description),
-            false)
+    if info and info.blueprintId ~= "unknown" then
+
+        local bpId = info.blueprintId
+        local text = LOC(__blueprints[bpId].General.UnitName)
+        
+        if text then
+            text = "\n" .. text .. " — " .. LOC(__blueprints[bpId].Interface.HelpText)
+        else
+            text = "\n" .. LOC(__blueprints[bpId].Interface.HelpText)
+        end
+
+        SetWeaponPriorities("{categories." .. bpId .. "}", text, false)
     end
 end
 -- Specific
-KeyMapper.SetUserKeyAction('target_specific', {
-    action = 'UI_Lua import("/lua/keymap/misckeyactions.lua").SetWeaponPrioritiesSpecific()',
-    category = 'Target priorities',
+KeyMapper.SetUserKeyAction("target_specific", {
+    action = "UI_Lua import(\"/lua/keymap/misckeyactions.lua\").SetWeaponPrioritiesSpecific()",
+    category = "Target priorities",
     order = 109
 })
-KeyMapper.SetUserKeyAction('Shift_target_specific', {
-    action = 'UI_Lua import("/lua/keymap/misckeyactions.lua").SetWeaponPrioritiesSpecific()',
-    category = 'Target priorities',
+KeyMapper.SetUserKeyAction("Shift_target_specific", {
+    action = "UI_Lua import(\"/lua/keymap/misckeyactions.lua\").SetWeaponPrioritiesSpecific()",
+    category = "Target priorities",
     order = 110
 })
