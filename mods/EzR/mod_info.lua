0f7f00f4eb84ada08d80dfa43e41ac0e2077be1a
4z0t <gastec139@gmail.com>
2022-06-04 00:20:01 +0300

    EzR: adjust mod info

--- mods/EzR/mod_info.lua
+++ mods/EzR/mod_info.lua
@@ -1,12 +1,13 @@
-name = "Ez Reclaim"
+name = "EzReclaim"
 version = 1
 copyright = ""
-description = "Shows reclaim labels in reclaim mode"
+description = "Shows reclaim labels in reclaim mode. Groups reclaim for better view."
 author = "4z0t"
 url = ""
-uid = "Ez-Reclaim-v01"
+uid = "EzReclaim-4z0t-v01"
 exclusive = false
 ui_only = true
-conflicts = {"2019eaac-e8ed-11e4-b02c-1681e6b88ec6", -- Advanced Reclaim Info
-"2018eaac-e8ed-11e4-b02c-1681e6b44c7" -- Selection Cost UI
+conflicts = {
+    "2019eaac-e8ed-11e4-b02c-1681e6b88ec6", -- Advanced Reclaim Info
+    "2018eaac-e8ed-11e4-b02c-1681e6b44c7" -- Selection Cost UI
 }
