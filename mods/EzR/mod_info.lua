80b8b50996b5b6a96f986176099d5db296f2beb8
4z0t <gastec139@gmail.com>
2022-06-03 15:41:30 +0300

    EzR: conflicts

--- mods/EzR/mod_info.lua
+++ mods/EzR/mod_info.lua
@@ -7,4 +7,6 @@ url = ""
 uid = "Ez-Reclaim-v01"
 exclusive = false
 ui_only = true
-conflicts = {}
+conflicts = {"2019eaac-e8ed-11e4-b02c-1681e6b88ec6", -- Advanced Reclaim Info
+"2018eaac-e8ed-11e4-b02c-1681e6b44c7" -- Selection Cost UI
+}
