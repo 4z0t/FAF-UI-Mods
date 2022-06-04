62e29107bd3f1a9ceb270fbc68dfdcd6613c61ee
4z0t <gastec139@gmail.com>
2022-06-04 19:43:16 +0300

    IEL: OptionVar logic

--- mods/IEL/modules/engineers.lua
+++ mods/IEL/modules/engineers.lua
@@ -1,40 +1,33 @@
-local LayoutHelpers = import('/lua/maui/layouthelpers.lua')
-local Bitmap = import('/lua/maui/bitmap.lua').Bitmap
-local Group = import('/lua/maui/group.lua').Group
-local UIUtil = import('/lua/ui/uiutil.lua')
-local Prefs = import('/lua/user/prefs.lua')
-local AddBeatFunction = import('/lua/ui/game/gamemain.lua').AddBeatFunction
-local worldView = import('/lua/ui/game/worldview.lua').viewLeft
-local LazyVar = import('/lua/lazyvar.lua')
+local LayoutHelpers = import("/lua/maui/layouthelpers.lua")
+local Bitmap = import("/lua/maui/bitmap.lua").Bitmap
+local Group = import("/lua/maui/group.lua").Group
+local UIUtil = import("/lua/ui/uiutil.lua")
+local Prefs = import("/lua/user/prefs.lua")
+local AddBeatFunction = import("/lua/ui/game/gamemain.lua").AddBeatFunction
+local worldView = import("/lua/ui/game/worldview.lua").viewLeft
+local LazyVar = import("/lua/lazyvar.lua")
 
+local GetUnits = import("/mods/UMT/modules/units.lua").Get
+local GlobalOptions = import("/mods/UMT/modules/GlobalOptions.lua")
+local OptionsUtils = import("/mods/UMT/modules/OptionsWindow.lua")
+local OptionVarCreate = import("/mods/UMT/modules/OptionVar.lua").Create
 
-local GetUnits = import('/mods/UMT/modules/units.lua').Get
-local GlobalOptions = import('/mods/UMT/modules/GlobalOptions.lua')
-local OptionsUtils = import('/mods/UMT/modules/OptionsWindow.lua')
+local modName = "IEL"
 
-local options = Prefs.GetFromCurrentProfile('IEL') or {
-    engineersOverlay = 1,
-    factoriesOverlay = 1,
-    supportCommanderOverlay = 1,
-    tacticalNukesOverlay = 1,
-    massExtractorsOverlay = 1
-}
+local engineersOption = OptionVarCreate(modName, "engineersOverlay", true)
+local factoriesOption = OptionVarCreate(modName, "factoriesOverlay", true)
+local supportCommanderOption = OptionVarCreate(modName, "supportCommanderOverlay", true)
+local tacticalNukesOption = OptionVarCreate(modName, "tacticalNukesOverlay", true)
+local massExtractorsOption = OptionVarCreate(modName, "massExtractorsOverlay", true)
 
-local engineersOverlay = options['engineersOverlay'] or 1
-local factoriesOverlay = options['factoriesOverlay'] or 1
-local supportCommanderOverlay = options['supportCommanderOverlay'] or 1
-local tacticalNukesOverlay = options['tacticalNukesOverlay'] or 1
-local massExtractorsOverlay = options['massExtractorsOverlay'] or 1
-
-local engineersOption = LazyVar.Create(engineersOverlay)
-local factoriesOption = LazyVar.Create(factoriesOverlay)
-local supportCommanderOption = LazyVar.Create(supportCommanderOverlay)
-local tacticalNukesOption = LazyVar.Create(tacticalNukesOverlay)
-local massExtractorsOption = LazyVar.Create(massExtractorsOverlay)
+local engineersOverlay = engineersOption()
+local factoriesOverlay = factoriesOption()
+local supportCommanderOverlay = supportCommanderOption()
+local tacticalNukesOverlay = tacticalNukesOption()
+local massExtractorsOverlay = massExtractorsOption()
 
 local overlays = {}
-local overlayGroup 
-
+local overlayGroup
 
 function CreateUnitOverlayControl(unit)
     -- creates an empty overlay control for a unit
@@ -77,17 +70,17 @@ function CreateEngineerOverlay(unit)
     overlays[id] = CreateUnitOverlayControl(unit)
     local overlay = overlays[id]
     if (unit:IsInCategory("TECH1")) then
-        overlay:SetTexture('/mods/IEL/textures/t1_idle_bold.dds', 0)
+        overlay:SetTexture("/mods/IEL/textures/t1_idle_bold.dds", 0)
     elseif (unit:IsInCategory("TECH2")) then
-        overlay:SetTexture('/mods/IEL/textures/t2_idle_bold.dds', 0)
+        overlay:SetTexture("/mods/IEL/textures/t2_idle_bold.dds", 0)
     elseif (unit:IsInCategory("TECH3")) then
-        overlay:SetTexture('/mods/IEL/textures/t3_idle_bold.dds', 0)
+        overlay:SetTexture("/mods/IEL/textures/t3_idle_bold.dds", 0)
     end
     overlay.OnFrame = OnFrameEngineer
 end
 
 function OnFrameEngineer(self, delta)
-    if (not self.unit:IsDead()) and engineersOverlay ~= 0 then
+    if (not self.unit:IsDead()) and engineersOverlay then
         if (self.unit:IsIdle()) then
             self:Update()
         else
@@ -110,13 +103,13 @@ function CreateFactoryOverlay(unit)
     -- LayoutHelpers.SetDimensions(overlay,12,12)
     -- overlay.Width:Set(12)
     -- overlay.Height:Set(12)
-    overlay:SetTexture({'/mods/IEL/textures/repeat.dds', '/mods/IEL/textures/idle_fac.dds',
-                        '/mods/IEL/textures/upgrading.dds'})
+    overlay:SetTexture({"/mods/IEL/textures/repeat.dds", "/mods/IEL/textures/idle_fac.dds",
+                        "/mods/IEL/textures/upgrading.dds"})
     LayoutHelpers.SetDimensions(overlay, 8, 8)
     overlay.OnFrame = OnFrameFactory
 end
 function OnFrameFactory(self, delta)
-    if (not self.unit:IsDead()) and factoriesOverlay ~= 0 then
+    if (not self.unit:IsDead()) and factoriesOverlay then
         if (self.unit:IsIdle()) then
             self:SetFrame(1)
             -- LayoutHelpers.SetDimensions(self,8,8)
@@ -147,13 +140,13 @@ function CreateSiloOverlay(unit)
     -- LayoutHelpers.SetDimensions(overlay,12,12)
     -- overlay.Width:Set(12)
     -- overlay.Height:Set(12)
-    overlay:SetTexture('/mods/IEL/textures/loaded.dds', 0)
+    overlay:SetTexture("/mods/IEL/textures/loaded.dds", 0)
     LayoutHelpers.SetDimensions(overlay, 12, 12)
     overlay.OnFrame = OnFrameSilo
 end
 
 function OnFrameSilo(self, delta)
-    if (not self.unit:IsDead()) and tacticalNukesOverlay ~= 0 then
+    if (not self.unit:IsDead()) and tacticalNukesOverlay then
         local mi = self.unit:GetMissileInfo()
         if (mi.nukeSiloStorageCount > 0) or (mi.tacticalSiloStorageCount > 0) then
             self:Update()
@@ -172,13 +165,13 @@ function CreateMexOverlay(unit)
 
     overlay.offsetX = 4
     overlay.offsetY = -8
-    overlay:SetTexture('/mods/IEL/textures/up.dds', 0)
+    overlay:SetTexture("/mods/IEL/textures/up.dds", 0)
     LayoutHelpers.SetDimensions(overlay, 12, 16)
     overlay.OnFrame = OnFrameMex
 end
 
 function OnFrameMex(self, delta)
-    if (not self.unit:IsDead()) and massExtractorsOverlay ~= 0 then
+    if (not self.unit:IsDead()) and massExtractorsOverlay then
         if self.unit:GetWorkProgress() > 0 then
             self:Update()
         else
@@ -192,7 +185,7 @@ end
 function VerifyWV()
     if IsDestroyed(worldView) -- ~= import('/lua/ui/game/worldview.lua').viewLeft 
     then
-        worldView = import('/lua/ui/game/worldview.lua').viewLeft
+        worldView = import("/lua/ui/game/worldview.lua").viewLeft
         overlays = {}
     end
 end
@@ -202,16 +195,15 @@ function CreateUnitOverlays()
     VerifyWV()
     for _, unit in allunits do
         if (not unit:IsDead()) and not overlays[unit:GetEntityId()] then
-            if engineersOverlay ~= 0 and unit:IsInCategory("ENGINEER") then
+            if engineersOverlay and unit:IsInCategory("ENGINEER") then
                 CreateEngineerOverlay(unit)
             elseif factoriesOverlay ~= 0 and unit:IsInCategory("FACTORY") then
                 CreateFactoryOverlay(unit)
-            elseif supportCommanderOverlay ~= 0 and unit:IsInCategory("SUBCOMMANDER") then
+            elseif supportCommanderOverlay and unit:IsInCategory("SUBCOMMANDER") then
 
-            elseif tacticalNukesOverlay ~= 0 and unit:IsInCategory("SILO") then
+            elseif tacticalNukesOverlay and unit:IsInCategory("SILO") then
                 CreateSiloOverlay(unit)
-            elseif massExtractorsOverlay ~= 0 and unit:IsInCategory("MASSEXTRACTION") and
-                (unit:IsInCategory("STRUCTURE")) then
+            elseif massExtractorsOverlay and unit:IsInCategory("MASSEXTRACTION") and (unit:IsInCategory("STRUCTURE")) then
                 CreateMexOverlay(unit)
             end
         end
@@ -221,7 +213,6 @@ end
 function UpdateOverlays()
 
 end
-
 
 function initOverlayGroup()
     overlayGroup = Group(worldView)
@@ -233,31 +224,30 @@ function initOverlayGroup()
 end
 
 function init(isReplay)
-    
 
     initOverlayGroup()
     AddBeatFunction(CreateUnitOverlays, true)
-    engineersOption.OnDirty = function(self)
+    engineersOption.OnChange = function(self)
         engineersOverlay = self()
     end
-    factoriesOption.OnDirty = function(self)
+    factoriesOption.OnChange = function(self)
         factoriesOverlay = self()
     end
-    supportCommanderOption.OnDirty = function(self)
+    supportCommanderOption.OnChange = function(self)
         supportCommanderOverlay = self()
     end
-    tacticalNukesOption.OnDirty = function(self)
+    tacticalNukesOption.OnChange = function(self)
         tacticalNukesOverlay = self()
     end
-    massExtractorsOption.OnDirty = function(self)
+    massExtractorsOption.OnChange = function(self)
         massExtractorsOverlay = self()
     end
 
-    GlobalOptions.AddOptions('IEL', 'Idle Engineers Light',
-        {OptionsUtils.Filter('Show engineers ovelays', 'engineersOverlay', engineersOption),
-         OptionsUtils.Filter('Show factories ovelays', 'factoriesOverlay', factoriesOption),
-         OptionsUtils.Filter('Show Nukes and TMLs ovelays', 'tacticalNukesOverlay', tacticalNukesOption),
-         OptionsUtils.Filter('Show Mex ovelays', 'massExtractorsOverlay', massExtractorsOption)})
+    GlobalOptions.AddOptions(modName, "Idle Engineers Light",
+        {OptionsUtils.Filter("Show engineers ovelays", engineersOption),
+         OptionsUtils.Filter("Show factories ovelays", factoriesOption),
+         OptionsUtils.Filter("Show Nukes and TMLs ovelays", tacticalNukesOption),
+         OptionsUtils.Filter("Show Mex ovelays", massExtractorsOption)})
 
 end
 
