e97999a758275128753f74274555dfc75d0a05cd
4z0t <84857900+4z0t@users.noreply.github.com>
2022-06-10 11:32:38 +0300

    EUT Version 3 (#5)
    * Selection on screen
    
    * Upgrade function
    
    * Mex overlays
    
    * GetUnits from UMT
    
    * Hide with whole UI
    
    * Apply LayoutFor

--- mods/EUT/modules/main.lua
+++ mods/EUT/modules/main.lua
@@ -1,7 +1,16 @@
-local MexManager = import('mexmanager.lua')
-local MexPanel = import('mexpanel.lua')
-
-function main()
-    MexManager.init()
-    MexPanel.init()
+function main(parent)
+    if exists("/mods/UMT/mod_info.lua") and import("/mods/UMT/mod_info.lua").version >= 4 then
+        local MexManager = import("mexmanager.lua")
+        local MexPanel = import("mexpanel.lua")
+        local MexOverlay = import("mexoverlay.lua")
+        MexManager.init()
+        MexPanel.init(parent)
+        MexOverlay.init()
+    else
+        ForkThread(function()
+            WaitSeconds(4)
+            print("ECO UI tools requires UI mod tools version 4 and higher!!!")
+        end)
+        return
+    end
 end
