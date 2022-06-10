aec84c6499afd9fc4f504c27c53975f68b086d49
4z0t <gastec139@gmail.com>
2022-06-10 10:51:46 +0300

    EUT: Version 3

--- mods/EUT/mod_info.lua
+++ mods/EUT/mod_info.lua
@@ -1,12 +1,12 @@
 name = "ECO UI Tools"
-version = 2
+version = 3
 copyright = ""
 description = [[Provides ECO management UI components.
 Requires UI mod tools.
 ]]
 author = "4z0t"
 url = ""
-uid = "eco-ui-tools-4z0t-v02"
+uid = "eco-ui-tools-4z0t-v03"
 exclusive = false
 ui_only = true
 conflicts = {  }
