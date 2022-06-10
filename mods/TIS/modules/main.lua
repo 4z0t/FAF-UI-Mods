47ba50e3b9e9cfefec1196b130f2fa62dc727e82
4z0t <gastec139@gmail.com>
2022-06-10 11:06:56 +0300

    TIS: update.lua

--- mods/TIS/modules/main.lua
+++ mods/TIS/modules/main.lua
@@ -9,6 +9,8 @@ local LayoutHelpers = import('/lua/maui/layouthelpers.lua')
 
 local LazyVar = import('/lua/lazyvar.lua')
 
+
+
 local options = Prefs.GetFromCurrentProfile('TIS') or {
     expOverlay = 1,
     siloOverlay = 1,
@@ -55,20 +57,20 @@ local siloOptions = {
 function AtCenterInOffset(control, parent)
     control.Left:Set(function()
         return math.floor(parent.Left() +
-                              (((parent.Width() / 2) - (control.Width() / 2)) +
-                                  LayoutHelpers.ScaleNumber(control.offsetX())))
+            (((parent.Width() / 2) - (control.Width() / 2)) +
+                LayoutHelpers.ScaleNumber(control.offsetX())))
     end)
     control.Top:Set(function()
         return math.floor(parent.Top() +
-                              (((parent.Height() / 2) - (control.Height() / 2)) +
-                                  LayoutHelpers.ScaleNumber(control.offsetY())))
+            (((parent.Height() / 2) - (control.Height() / 2)) +
+                LayoutHelpers.ScaleNumber(control.offsetY())))
     end)
 end
+
 local Units
 local listeners = {}
 local function UpdateUnitsListeners()
-    local units = Units.Get() -- dispose of Common mod tools dep
-    -- implement callback system
+    local units = Units.Get()
     for _, unit in units do
         if (not unit:IsDead()) then
             if not listeners[unit:GetEntityId()] then
@@ -95,15 +97,7 @@ function Remove(id)
 end
 
 function init(isReplay)
-    if not exists('/mods/UMT/modules/GlobalOptions.lua') or not exists('/mods/common/units.lua') then
-        ForkThread(function()
-            WaitSeconds(4)
-            for i = 1, 10 do
-                print("TeamInfo Share requires UI mod tools and Common Mod tools!!!")
-            end
-        end)
-        return
-    else
+    if exists('/mods/UMT/mod_info.lua') and import('/mods/UMT/mod_info.lua').version >= 4 then
         local GlobalOptions = import('/mods/UMT/modules/GlobalOptions.lua')
         local OptionsUtils = import('/mods/UMT/modules/OptionsWindow.lua')
 
@@ -114,26 +108,31 @@ function init(isReplay)
         Nuke.init(isReplay, siloOptions)
         Units = import('/mods/common/units.lua')
         GlobalOptions.AddOptions('TIS', 'TeamInfo Share',
-            {OptionsUtils.Filter('Show exp ovelays', 'expOverlay', expOptions.overlay),
-             OptionsUtils.Filter('Show nukes and smds ovelays', 'siloOverlay', siloOptions.overlay),
-             OptionsUtils.Title('Nukes and smds', 18, nil, UIUtil.factionTextColor),
-             OptionsUtils.Title('Countdown', 12),
-             OptionsUtils.Slider('Vertical offset', 'NVOffsetCountdown', -20, 20, 1, siloOptions.eta.offsetY),
-             OptionsUtils.Slider('Horizonal offset', 'NHOffsetCountdown', -20, 20, 1, siloOptions.eta.offsetX),
-             OptionsUtils.Title('Progress', 12),
-             OptionsUtils.Slider('Vertical offset', 'NVOffsetProgress', -20, 20, 1, siloOptions.progress.offsetY),
-             OptionsUtils.Slider('Horizonal offset', 'NHOffsetProgress', -20, 20, 1, siloOptions.progress.offsetX),
-             OptionsUtils.Title('Silo count', 12),
-             OptionsUtils.Slider('Vertical offset', 'NVOffsetCount', -20, 20, 1, siloOptions.count.offsetY),
-             OptionsUtils.Slider('Horizonal offset', 'NHOffsetCount', -20, 20, 1, siloOptions.count.offsetX),
-             OptionsUtils.Title('EXPs', 18, nil, UIUtil.factionTextColor), OptionsUtils.Title('Countdown', 12),
-             OptionsUtils.Slider('Vertical offset', 'EVOffsetCountdown', -20, 20, 1, expOptions.eta.offsetY),
-             OptionsUtils.Slider('Horizonal offset', 'EHOffsetCountdown', -20, 20, 1, expOptions.eta.offsetX),
-             OptionsUtils.Title('Progress', 12),
-             OptionsUtils.Slider('Vertical offset', 'EVOffsetProgress', -20, 20, 1, expOptions.progress.offsetY),
-             OptionsUtils.Slider('Horizonal offset', 'EHOffsetProgress', -20, 20, 1, expOptions.progress.offsetX)})
+            { OptionsUtils.Filter('Show exp ovelays', 'expOverlay', expOptions.overlay),
+                OptionsUtils.Filter('Show nukes and smds ovelays', 'siloOverlay', siloOptions.overlay),
+                OptionsUtils.Title('Nukes and smds', 18, nil, UIUtil.factionTextColor),
+                OptionsUtils.Title('Countdown', 12),
+                OptionsUtils.Slider('Vertical offset', 'NVOffsetCountdown', -20, 20, 1, siloOptions.eta.offsetY),
+                OptionsUtils.Slider('Horizonal offset', 'NHOffsetCountdown', -20, 20, 1, siloOptions.eta.offsetX),
+                OptionsUtils.Title('Progress', 12),
+                OptionsUtils.Slider('Vertical offset', 'NVOffsetProgress', -20, 20, 1, siloOptions.progress.offsetY),
+                OptionsUtils.Slider('Horizonal offset', 'NHOffsetProgress', -20, 20, 1, siloOptions.progress.offsetX),
+                OptionsUtils.Title('Silo count', 12),
+                OptionsUtils.Slider('Vertical offset', 'NVOffsetCount', -20, 20, 1, siloOptions.count.offsetY),
+                OptionsUtils.Slider('Horizonal offset', 'NHOffsetCount', -20, 20, 1, siloOptions.count.offsetX),
+                OptionsUtils.Title('EXPs', 18, nil, UIUtil.factionTextColor), OptionsUtils.Title('Countdown', 12),
+                OptionsUtils.Slider('Vertical offset', 'EVOffsetCountdown', -20, 20, 1, expOptions.eta.offsetY),
+                OptionsUtils.Slider('Horizonal offset', 'EHOffsetCountdown', -20, 20, 1, expOptions.eta.offsetX),
+                OptionsUtils.Title('Progress', 12),
+                OptionsUtils.Slider('Vertical offset', 'EVOffsetProgress', -20, 20, 1, expOptions.progress.offsetY),
+                OptionsUtils.Slider('Horizonal offset', 'EHOffsetProgress', -20, 20, 1, expOptions.progress.offsetX) })
         -- Smd.init(isReplay)
         AddBeatFunction(UpdateUnitsListeners, true)
+    else
+        ForkThread(function()
+            WaitSeconds(4)
+            print("TeamInfo Share requires UI mod tools version 4 and higher!!!")
+        end)
+        return
     end
-
 end
