f34398a9fb784938addf920da3b4f25b506a03f7
4z0t <gastec139@gmail.com>
2022-06-14 00:08:08 +0300

    AOE: Select nearest air scout / build intel sensors

--- mods/AOE/hook/lua/keymap/misckeyactions.lua
+++ mods/AOE/hook/lua/keymap/misckeyactions.lua
@@ -1,13 +1,16 @@
 local KeyMapper = import("/lua/keymap/keymapper.lua")
 
+
+-- Select ACU / goto ACU / OC mode
 local lastACUSelectionTime = 0
+local EnterOverchargeMode = import("/lua/ui/game/orders.lua").EnterOverchargeMode
 function ACUSelectOCCG()
     local curTime = GetSystemTimeSeconds()
     local diffTime = curTime - lastACUSelectionTime
     if diffTime > 1.0 then
         local selection = GetSelectedUnits()
         if not table.empty(selection) and table.getn(selection) == 1 and selection[1]:IsInCategory "COMMAND" then
-            ConExecute "UI_Lua import(\"/lua/ui/game/orders.lua\").EnterOverchargeMode()"
+            EnterOverchargeMode()
         else
             ConExecute "UI_SelectByCategory +nearest COMMAND"
         end
@@ -17,6 +20,7 @@ function ACUSelectOCCG()
     lastACUSelectionTime = curTime
 end
 
+-- Select nearest idle engineer / Reclaim mode
 function ReclaimSelectIDLENearestT1()
     local selection = GetSelectedUnits()
     if table.empty(selection) then
@@ -26,19 +30,31 @@ function ReclaimSelectIDLENearestT1()
     end
 end
 
+-- Decrease Unit count in factory queue
 local DecreaseBuildCountInQueue = import("/lua/ui/game/construction.lua").DecreaseBuildCountInQueue
 local RefreshUI = import("/lua/ui/game/construction.lua").RefreshUI
 function RemoveLastItem()
     local selectedUnits = GetSelectedUnits()
-    if selectedUnits and selectedUnits[1]:IsInCategory("FACTORY") then
+    if selectedUnits and selectedUnits[1]:IsInCategory "FACTORY" then
         local currentCommandQueue = SetCurrentFactoryForQueueDisplay(selectedUnits[1])
         local count = 1
-        if IsKeyDown("shift") then
+        if IsKeyDown "Shift" then
             count = 5
         end
         DecreaseBuildCountInQueue(table.getsize(currentCommandQueue), count)
         ClearCurrentFactoryForQueueDisplay()
         RefreshUI()
+    end
+end
+
+-- Select nearest air scout / build sensors
+local BuildAction = import("/lua/keymap/hotbuild.lua").buildAction
+function SelectAirScoutBuildIntel()
+    local selectedUnits = GetSelectedUnits()
+    if selectedUnits then
+        BuildAction "Sensors"
+    else
+        ConExecute "UI_SelectByCategory +nearest AIR INTELLIGENCE"
     end
 end
 
@@ -76,4 +92,17 @@ KeyMapper.SetUserKeyAction("SHIFT Select Nearest IDLE T1 engineer / enter reclai
     action = "UI_Lua import(\"/lua/keymap/misckeyactions.lua\").ReclaimSelectIDLENearestT1()",
     category = "selection",
     order = 22
+})
+
+
+KeyMapper.SetUserKeyAction("Select nearest air scout / build sensors", {
+    action = "UI_Lua import(\"/lua/keymap/misckeyactions.lua\").SelectAirScoutBuildIntel()",
+    category = "selection",
+    order = 23
+})
+
+KeyMapper.SetUserKeyAction("SHIFT Select nearest air scout / build sensors", {
+    action = "UI_Lua import(\"/lua/keymap/misckeyactions.lua\").SelectAirScoutBuildIntel()",
+    category = "selection",
+    order = 24
 })
