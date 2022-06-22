296e239385f2e450388f43cf918896000741821e
4z0t <gastec139@gmail.com>
2022-06-22 14:53:49 +0300

    4SB: asserts for errors

--- mods/4SB/modules/Animations/AnimationFactory.lua
+++ mods/4SB/modules/Animations/AnimationFactory.lua
@@ -33,20 +33,16 @@ local BaseAnimationFactory = ClassSimple
     end,
 
     Create = function(self)
-        if self._onStart and
-            self._onFrame and
-            self._onFinish then
+        assert(self._onStart and self._onFrame and self._onFinish, "Not complete animation")
 
-            local animation = Animation.Create(self._onStart, self._onFrame, self._onFinish)
+        local animation = Animation.Create(self._onStart, self._onFrame, self._onFinish)
 
-            self._onStart = false
-            self._onFrame = false
-            self._onFinish = false
+        self._onStart = false
+        self._onFrame = false
+        self._onFinish = false
 
-            return animation
-        else
-            error("Not complete animation")
-        end
+        return animation
+
     end
 }
 
@@ -74,9 +70,8 @@ local AlphaAnimationFactory = Class(BaseAnimationFactory)
     end,
 
     For = function(self, duration)
-        if duration <= 0 then
-            error("Duration of alpha animation cant be negative or 0")
-        end
+        assert(duration > 0, "Duration of alpha animation cant be negative or 0")
+
         self._duration = duration
         return self
     end,
@@ -87,18 +82,15 @@ local AlphaAnimationFactory = Class(BaseAnimationFactory)
     end,
 
     Create = function(self)
-        if not (self._endAlpha and self._direction and self._duration) then
-            error("Not complete Alpha animation")
-        end
+        assert(self._endAlpha and self._direction and self._duration, "Not complete Alpha animation")
+
         if self._direction == 1 then
             self._startAlpha = self._startAlpha or 0
         else
             self._startAlpha = self._startAlpha or 1
         end
 
-        if (self._endAlpha - self._startAlpha) * self._direction <= 0 then
-            error("End and Start alphas cant be same and must be correct with Appear/Fade")
-        end
+        assert((self._endAlpha - self._startAlpha) * self._direction > 0, "End and Start alphas cant be same and must be correct with Appear/Fade")
 
         local startAlpha = self._startAlpha
         local endAlpha = self._endAlpha
