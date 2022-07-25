206b5e2ba1c277237d5630f66475a99471b48b32
4z0t <gastec139@gmail.com>
2022-07-25 17:25:03 +0300

    EUT: auto t1 mex upgrade

---
+++ mods/EUT/modules/options.lua
@@ -0,0 +1,14 @@
+local GlobalOptions = import("/mods/UMT/modules/GlobalOptions.lua")
+local Options = import("/mods/UMT/modules/OptionsWindow.lua")
+local OptionVar = import("/mods/UMT/modules/OptionVar.lua").Create
+
+overlayOption = OptionVar("EUT", "MexOverlay", true)
+upgradeT1Option = OptionVar("EUT", "UpgradeT1", true)
+
+function Init()
+    GlobalOptions.AddOptions("EUT", "ECO UI Tools", {
+        Options.Filter("Show mex overlay", overlayOption),
+        Options.Filter("Auto T1 mex upgrade", upgradeT1Option),
+    })
+end
+
