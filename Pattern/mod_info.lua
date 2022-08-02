dad88dfb1b0cd7bbc0cf8f327d87d9f5268789ff
4z0t <gastec139@gmail.com>
2022-08-02 15:53:54 +0300

    UI mod pattern

---
+++ Pattern/mod_info.lua
@@ -0,0 +1,17 @@
+name = "your mod name"
+version = 1
+copyright = ""
+description = "your description"
+author = "you"
+url = ""
+uid = "your-mod-uid"
+icon = "/mods/Pattern/icon.png"
+selectable = true
+enabled = true
+exclusive = false
+ui_only = true
+requires = {}
+requiresNames = {}
+conflicts = {}
+before = {}
+after = {}
