3de1afabf26d8932b46f8cc7fb2056427cf5d19c
Alexander <84857900+4z0t@users.noreply.github.com>
2022-08-20 22:00:17 +0300

    SUI Version 1 (#14)
    * SUI: init
    
    * SUI: change texture for horizonal cursor
    
    * SUI: hide panel after creation
    
    * SUI: Update UI on tick
    
    * SUI: Display Enhancements costs
    
    * SUI: Add Silo for BuildRate accum
    
    * SUI: Icon
    
    * SUI: Version 1

--- mods/SUI/mod_info.lua
+++ mods/SUI/mod_info.lua
@@ -1,9 +1,12 @@
 name = "Selected Units Info"
 version = 1
 copyright = ""
-description = ""
+
+description = "Displays units cost, production/consumption and build power"
 author = "4z0t"
 url = ""
+icon = "/mods/SUI/icon.png"
+
 uid = "selected-units-info-v01"
 exclusive = false
 ui_only = true
