e56d9549c28712a2601f7b122736705f22252ac9
Alexander <84857900+4z0t@users.noreply.github.com>
2022-09-27 14:25:52 +0300

    Move hotkey set into main

--- mods/HBO/hook/lua/ui/game/gamemain.lua
+++ mods/HBO/hook/lua/ui/game/gamemain.lua
@@ -1,11 +1,4 @@
 do 
-    local KeyMapper = import('/lua/keymap/keymapper.lua')
-    KeyMapper.SetUserKeyAction('Open HBO editor', {
-        action = 'UI_Lua import("/mods/HBO/modules/views/view.lua").init()',
-        category = 'HotBuild Overhaul Editor',
-        order = 404
-    })
-    
     local originalCreateUI = CreateUI
     function CreateUI(isReplay, parent)
         originalCreateUI(isReplay)
--- mods/HBO/modules/main.lua
+++ mods/HBO/modules/main.lua
@@ -4,9 +4,18 @@ function Main(isReplay)
         local Model = import('model.lua')
         local View = import("views/view.lua")
         local Share = import("share.lua")
+        local KeyMapper = import('/lua/keymap/keymapper.lua')
+
+
         Model.init()
         ViewModel.init()
         Share.Init(isReplay)
+        KeyMapper.SetUserKeyAction('Open HBO editor', {
+            action = 'UI_Lua import("/mods/HBO/modules/views/view.lua").init()',
+            category = 'HotBuild Overhaul Editor',
+            order = 404
+        })
+
     else
         ForkThread(function()
             WaitSeconds(4)
