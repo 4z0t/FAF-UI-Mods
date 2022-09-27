81d5142476f405f120f6eaf1364ff0d7c4cc24a6
4z0t <gastec139@gmail.com>
2022-09-27 10:06:38 +0300

    Version 2

--- mods/EzR/hook/lua/ui/game/gamemain.lua
+++
@@ -1,5 +0,0 @@
--- local originalCreateUI = CreateUI
--- function CreateUI(isReplay, parent)
---     originalCreateUI(isReplay)
---     import("/mods/EzR/modules/main.lua").init(isReplay)
--- end
--- mods/EzR/mod_info.lua
+++ mods/EzR/mod_info.lua
@@ -1,13 +1,12 @@
 name = "EzReclaim"
-version = 1
+version = 2
 copyright = ""
-description = "Shows reclaim labels in reclaim mode. Groups reclaim for better view."
+description = "Shows reclaim labels in reclaim mode."
 author = "4z0t"
 url = ""
-uid = "EzReclaim-4z0t-v01"
+uid = "EzReclaim-4z0t-v02"
 exclusive = false
 ui_only = true
 conflicts = {
     "2019eaac-e8ed-11e4-b02c-1681e6b88ec6", -- Advanced Reclaim Info
-    "2018eaac-e8ed-11e4-b02c-1681e6b44c7" -- Selection Cost UI
 }
