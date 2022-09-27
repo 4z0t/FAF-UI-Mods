49f43efbbf476c2612c6fc92a3bd385858ced0a5
4z0t <gastec139@gmail.com>
2022-09-27 10:38:01 +0300

    optimize

--- mods/SRD/hook/lua/ui/game/gamemain.lua
+++ mods/SRD/hook/lua/ui/game/gamemain.lua
@@ -1,5 +1,8 @@
-local originalCreateUI = CreateUI
-function CreateUI(isReplay, parent)
-    originalCreateUI(isReplay)
-    import("/mods/SRD/modules/main.lua").init(isReplay)
+do 
+    local originalCreateUI = CreateUI
+    function CreateUI(isReplay)
+        originalCreateUI(isReplay)
+        import("/mods/SRD/modules/main.lua").init(isReplay)
+    end
 end
+
