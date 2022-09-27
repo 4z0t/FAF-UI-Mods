eb173d589dee9845cf7df22df4404d6a9ad0866c
Alexander <84857900+4z0t@users.noreply.github.com>
2022-09-27 14:17:23 +0300

    put in do end block

--- mods/HBO/hook/lua/ui/game/gamemain.lua
+++ mods/HBO/hook/lua/ui/game/gamemain.lua
@@ -1,13 +1,14 @@
-
-local KeyMapper = import('/lua/keymap/keymapper.lua')
-KeyMapper.SetUserKeyAction('Open HBO editor', {
-    action = 'UI_Lua import("/mods/HBO/modules/views/view.lua").init()',
-    category = 'HotBuild Overhaul Editor',
-    order = 404
-})
-
-local originalCreateUI = CreateUI
-function CreateUI(isReplay, parent)
-    originalCreateUI(isReplay)
-    import("/mods/HBO/modules/main.lua").init(isReplay)
+do 
+    local KeyMapper = import('/lua/keymap/keymapper.lua')
+    KeyMapper.SetUserKeyAction('Open HBO editor', {
+        action = 'UI_Lua import("/mods/HBO/modules/views/view.lua").init()',
+        category = 'HotBuild Overhaul Editor',
+        order = 404
+    })
+    
+    local originalCreateUI = CreateUI
+    function CreateUI(isReplay, parent)
+        originalCreateUI(isReplay)
+        import("/mods/HBO/modules/main.lua").init(isReplay)
+    end
 end
--- mods/HBO/hook/lua/ui/game/tabs.lua
+++ mods/HBO/hook/lua/ui/game/tabs.lua
@@ -1,9 +1,11 @@
-for _, t in menus.main do
-    table.insert(t, {
-        action = 'HBO',
-        label = 'HotBuild Overhaul',
-        tooltip = 'TODO'
-    })
-end
+do
+    for _, t in menus.main do
+        table.insert(t, {
+            action = 'HBO',
+            label = 'HotBuild Overhaul',
+            tooltip = 'TODO'
+        })
+    end
 
-actions['HBO'] = import("/mods/HBO/modules/views/view.lua").init
+    actions['HBO'] = import("/mods/HBO/modules/views/view.lua").init
+end
