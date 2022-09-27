88088d6cdc9920b1ad9a2de2d22c2aa76f96b3bb
4z0t <gastec139@gmail.com>
2022-09-27 10:30:09 +0300

    optimize

--- mods/EzR/hook/lua/ui/controls/worldview.lua
+++ mods/EzR/hook/lua/ui/controls/worldview.lua
@@ -1,33 +1,35 @@
-local GetCommandMode = import("/lua/ui/game/commandmode.lua").GetCommandMode
-local ShowReclaim = import("/lua/ui/game/Reclaim.lua").ShowReclaim
+do
+    local GetCommandMode = import("/lua/ui/game/commandmode.lua").GetCommandMode
+    local ShowReclaim = import("/lua/ui/game/Reclaim.lua").ShowReclaim
+    local oldWorldView = WorldView
+    WorldView = Class(oldWorldView) {
 
-local oldWorldView = WorldView
-WorldView = Class(oldWorldView) {
-    EnabledWithReclaimMode = false,
-    OnUpdateCursor = function(self)
-        local order = GetCommandMode()[2] and GetCommandMode()[2].name
-        if order == "RULEUCC_Reclaim" then
-            if not self.ReclaimThread then
-                ShowReclaim(true)
-            else
-                self.ShowingReclaim = true
+        EnabledWithReclaimMode = false,
+
+        OnUpdateCursor = function(self)
+            local order = GetCommandMode()[2] and GetCommandMode()[2].name
+            if order == "RULEUCC_Reclaim" then
+                if not self.ReclaimThread then
+                    ShowReclaim(true)
+                else
+                    self.ShowingReclaim = true
+                end
+                self.EnabledWithReclaimMode = true
+            elseif order ~= "RULEUCC_Move" and self.EnabledWithReclaimMode then
+                if not self.ReclaimThread then
+                    ShowReclaim(false)
+                else
+                    self.ShowingReclaim = false
+                end
+                self.EnabledWithReclaimMode = false
             end
-            self.EnabledWithReclaimMode = true
-        elseif order ~= "RULEUCC_Move" and self.EnabledWithReclaimMode then
-            if not self.ReclaimThread then
-                ShowReclaim(false)
-            else
-                self.ShowingReclaim = false
-            end
-            self.EnabledWithReclaimMode = false
+            return oldWorldView.OnUpdateCursor(self)
+        end,
+
+        OnDestroy = function(self)
+            ShowReclaim(false)
+            oldWorldView.OnDestroy(self)
         end
-
-        return oldWorldView.OnUpdateCursor(self)
-    end,
-
-    OnDestroy = function(self)
-        ShowReclaim(false)
-        oldWorldView.OnDestroy(self)
-    end
-
-}
+        
+    }
+end
