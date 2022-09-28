c151493454ceed96b2006928ff09178c411c825b
Alexander <84857900+4z0t@users.noreply.github.com>
2022-09-28 12:09:32 +0300

    EzR Version 2 (#12)
    * EzR: init
    
    * EzR: OnCommandGraphShow hook
    
    * EzR: conflicts
    
    * EzR: conflicts
    
    * EzR: fix reclaim update
    
    * EzR: import function
    
    * EzR: description
    
    * EzR: reclaim grouping
    
    * EzR: merge check
    
    * EzR: adjust mod info
    
    * EzR: adjust reclaim labels
    
    * EzR: cleanup
    
    * EzR: class based structure
    
    * Version 2
    
    * Add icon
    
    * optimize

---
+++ mods/EzR/hook/lua/ui/controls/worldview.lua
@@ -0,0 +1,35 @@
+do
+    local GetCommandMode = import("/lua/ui/game/commandmode.lua").GetCommandMode
+    local ShowReclaim = import("/lua/ui/game/Reclaim.lua").ShowReclaim
+    local oldWorldView = WorldView
+    WorldView = Class(oldWorldView) {
+
+        EnabledWithReclaimMode = false,
+
+        OnUpdateCursor = function(self)
+            local order = GetCommandMode()[2] and GetCommandMode()[2].name
+            if order == "RULEUCC_Reclaim" then
+                if not self.ReclaimThread then
+                    ShowReclaim(true)
+                else
+                    self.ShowingReclaim = true
+                end
+                self.EnabledWithReclaimMode = true
+            elseif order ~= "RULEUCC_Move" and self.EnabledWithReclaimMode then
+                if not self.ReclaimThread then
+                    ShowReclaim(false)
+                else
+                    self.ShowingReclaim = false
+                end
+                self.EnabledWithReclaimMode = false
+            end
+            return oldWorldView.OnUpdateCursor(self)
+        end,
+
+        OnDestroy = function(self)
+            ShowReclaim(false)
+            oldWorldView.OnDestroy(self)
+        end
+        
+    }
+end
