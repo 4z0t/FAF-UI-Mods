3331578efc31164b5f550843eed3065ae46607e5
Alexander <84857900+4z0t@users.noreply.github.com>
2022-09-30 14:49:21 +0300

    annotate OptionVar

--- mods/UMT/modules/OptionVar.lua
+++ mods/UMT/modules/OptionVar.lua
@@ -1,13 +1,24 @@
 local LazyVar = import("/lua/lazyvar.lua")
 local Prefs = import("/lua/user/prefs.lua")
 
+
+---@class OptionVar
+---@field _m string
+---@field _o string
+---@field _lv LazyVar
+---@field _prev any
+---@field OnChange fun(self : OptionVar)
 local OptionVarMetaTable = {}
 OptionVarMetaTable.__index = OptionVarMetaTable
 
+---returns stored value in optionvar
+---@return any
 function OptionVarMetaTable:__call()
     return self._lv()
 end
 
+---sets new value for optionvar
+---@param value any
 function OptionVarMetaTable:Set(value)
     if self._prev == nil then
         self._prev = self._lv()
@@ -16,6 +27,7 @@ function OptionVarMetaTable:Set(value)
     self:OnChange()
 end
 
+---resets value to previous saved one
 function OptionVarMetaTable:Reset()
     if self._prev ~= nil then
         self:Set(self._prev)
@@ -23,6 +35,7 @@ function OptionVarMetaTable:Reset()
     end
 end
 
+---saves value stored in optionvar
 function OptionVarMetaTable:Save()
     local modOptionsTable = Prefs.GetFromCurrentProfile(self._m)
     modOptionsTable[self._o] = self._lv()
@@ -30,15 +43,23 @@ function OptionVarMetaTable:Save()
     self._prev = nil
 end
 
+---returns option name of optionvar
+---@return string
 function OptionVarMetaTable:Option()
     return self._o
 end
 
+---returns lazyvar nested in optionvar
+---@return LazyVar
 function OptionVarMetaTable:Raw()
     return self._lv
 end
 
-
+---creates optionvar with default value if there is no saved one with given name
+---@param modOptionName string
+---@param subOption string
+---@param default any
+---@return OptionVar
 function Create(modOptionName, subOption, default)
     local modOptionsTable = Prefs.GetFromCurrentProfile(modOptionName)
     local val = modOptionsTable and modOptionsTable[subOption]
