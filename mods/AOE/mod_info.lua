97a0b2796428a9e9039249db380e0dd552bcd3e9
4z0t <gastec139@gmail.com>
2022-10-01 20:44:50 +0300

    Version 3

--- mods/AOE/mod_info.lua
+++ mods/AOE/mod_info.lua
@@ -1,15 +1,21 @@
 name = "Additional Orders Extension"
-version = 2
+version = 3
 copyright = ""
 description = [[Additional orders combining selection and action and some QoL ones
 * Select ACU / goto ACU / OC mode
 * Select nearest idle engineer / Reclaim mode
 * Decrease Unit count in factory queue
 * Select nearest air scout / build sensors
-* Select All IDLE engineers on screen not ACU]]
+* Select All IDLE engineers on screen not ACU
+* Select nearest transport / transport order
+* Select nearest air scout / build intel sensors
+* Select all sensors
+* Select all radars
+* Upgrade mex, select next idle nearest
+* Loop over idle mexes]]
 author = "4z0t"
 url = ""
-uid = "additional-orders-extension-v02"
+uid = "additional-orders-extension-v03"
 exclusive = false
 ui_only = true
 conflicts = {}
