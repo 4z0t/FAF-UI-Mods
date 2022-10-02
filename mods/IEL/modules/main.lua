93be4f359282e73a10b1c93cae351b62b7bfebe5
4z0t <gastec139@gmail.com>
2022-10-02 21:46:01 +0300

    new version for UMT

--- mods/IEL/modules/main.lua
+++ mods/IEL/modules/main.lua
@@ -1,11 +1,11 @@
 function Main(isReplay)
-    if exists("/mods/UMT/mod_info.lua") and import("/mods/UMT/mod_info.lua").version >= 4 then
+    if exists("/mods/UMT/mod_info.lua") and import("/mods/UMT/mod_info.lua").version >= 6 then
         import("options.lua").Main(isReplay)
         import("engineers.lua").Init(isReplay)
     else
         ForkThread(function()
             WaitSeconds(4)
-            print("Idle Engineers Light requires UI mod tools version 4 and higher!!!")
+            print("Idle Engineers Light requires UI mod tools version 6 and higher!!!")
         end)
         return
     end
