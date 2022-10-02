db4ef393d7f13f3dfb483ac90d49665f94146789
Alexander <84857900+4z0t@users.noreply.github.com>
2022-10-02 21:51:57 +0300

    IEL Version 9 (#18)
    * OptionVar logic
    * display factory building engi in repeat queue
    * refactor overlays

--- mods/IEL/modules/main.lua
+++ mods/IEL/modules/main.lua
@@ -1,10 +1,12 @@
-function main(isReplay)
-    if exists("/mods/UMT/mod_info.lua") and import("/mods/UMT/mod_info.lua").version >= 4 then
-        import("engineers.lua").init(isReplay)
+
+function Main(isReplay)
+    if exists("/mods/UMT/mod_info.lua") and import("/mods/UMT/mod_info.lua").version >= 6 then
+        import("options.lua").Main(isReplay)
+        import("engineers.lua").Init(isReplay)
     else
         ForkThread(function()
             WaitSeconds(4)
-            print("Idle Engineers Light requires UI mod tools version 4 and higher!!!")
+            print("Idle Engineers Light requires UI mod tools version 6 and higher!!!")
         end)
         return
     end
