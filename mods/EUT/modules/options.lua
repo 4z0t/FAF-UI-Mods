54d1c210ff251463af87d9c1c80620ffa143156e
4z0t <gastec139@gmail.com>
2022-10-03 12:37:40 +0300

    add mod name

--- mods/EUT/modules/options.lua
+++ mods/EUT/modules/options.lua
@@ -3,12 +3,14 @@ do
     local Options = import("/mods/UMT/modules/OptionsWindow.lua")
     local OptionVar = import("/mods/UMT/modules/OptionVar.lua").Create
 
-    overlayOption = OptionVar("EUT", "MexOverlay", true)
-    useNumberOverlay = OptionVar("EUT", "NumberOverlay", false)
-    upgradeT1Option = OptionVar("EUT", "UpgradeT1", false)
+    local modName = "EUT"
+
+    overlayOption = OptionVar(modName, "MexOverlay", true)
+    useNumberOverlay = OptionVar(modName, "NumberOverlay", false)
+    upgradeT1Option = OptionVar(modName, "UpgradeT1", false)
 
     function Init()
-        GlobalOptions.AddOptions("EUT", "ECO UI Tools", {
+        GlobalOptions.AddOptions(modName, "ECO UI Tools", {
             Options.Filter("Show mex overlay", overlayOption),
             Options.Filter("Auto T1 mex upgrade", upgradeT1Option),
             Options.Filter("Display mex overlay with numbers", useNumberOverlay),
