b0cb8834a2d6384e53dadb80ece8abb61eeca68b
4z0t <gastec139@gmail.com>
2022-10-08 21:22:21 +0300

    cleanup

--- mods/UMT/modules/main.lua
+++ mods/UMT/modules/main.lua
@@ -1,7 +1,5 @@
-local From = import('linq.lua').from
-local Range = import('linq.lua').range
-import('Importer.lua')
 
-function init(isReplay)
-    --import('tests.lua').main()
+
+function Main(isReplay)
+    
 end
