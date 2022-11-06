88ad34cd5672a187b860b9c87d089c15b6c6ea65
4z0t <gastec139@gmail.com>
2022-11-06 21:34:54 +0300

    EUT Version 5

--- mods/EUT/mod_info.lua
+++ mods/EUT/mod_info.lua
@@ -1,12 +1,18 @@
 name = "ECO UI Tools"
-version = 4
+version = 5
 copyright = ""
 description = [[Provides ECO management UI components.
 Requires UI mod tools.
+
+Options:
+- Auto t1 mex upgrade
+- Auto t2 capped mex upgrade
+- mex overlay
+
 ]]
 author = "4z0t"
 url = ""
-uid = "eco-ui-tools-4z0t-v04"
+uid = "eco-ui-tools-4z0t-v05"
 exclusive = false
 ui_only = true
 conflicts = {}
