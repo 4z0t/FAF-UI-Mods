eeb398c879122e99a223e645fce60f6a787f6268
4z0t <gastec139@gmail.com>
2022-11-09 13:28:29 +0300

    EUT Version 6

--- mods/EUT/mod_info.lua
+++ mods/EUT/mod_info.lua
@@ -1,18 +1,20 @@
 name = "ECO UI Tools"
-version = 5
+version = 6
 copyright = ""
 description = [[Provides ECO management UI components.
-Requires UI mod tools.
 
 Options:
 - Auto t1 mex upgrade
 - Auto t2 capped mex upgrade
 - mex overlay
+- unpause while assist with given BP threshold
 
+You can find these options in menu > UI Mods Options > ECO UI Tools.
+Requires UI mod tools.
 ]]
 author = "4z0t"
 url = ""
-uid = "eco-ui-tools-4z0t-v05"
+uid = "eco-ui-tools-4z0t-v06"
 exclusive = false
 ui_only = true
 conflicts = {}
