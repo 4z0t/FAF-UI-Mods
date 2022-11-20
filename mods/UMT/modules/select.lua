63cb913e8c8df139a9e46e1f890129a4e158bb55
4z0t <gastec139@gmail.com>
2022-11-20 17:28:50 +0300

    remove duplicate

--- mods/UMT/modules/select.lua
+++ mods/UMT/modules/select.lua
@@ -1,14 +1,1 @@
-do
-    local SetIgnoreSelection = import('/lua/ui/game/gamemain.lua').SetIgnoreSelection
-    local CommandMode = import('/lua/ui/game/commandmode.lua')
-
-    function Hidden(callback)
-        local current_command = CommandMode.GetCommandMode()
-        local old_selection = GetSelectedUnits() or {}
-        SetIgnoreSelection(true)
-        callback()
-        SelectUnits(old_selection)
-        CommandMode.StartCommandMode(current_command[1], current_command[2])
-        SetIgnoreSelection(false)
-    end
-end
+Hidden = UMT.Select.Hidden
