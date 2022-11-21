eb0de83f8ce075cca55402adb0d467c606814cc7
4z0t <gastec139@gmail.com>
2022-11-21 16:53:15 +0300

    4SB Version 1

--- mods/4SB/mod_info.lua
+++ mods/4SB/mod_info.lua
@@ -1,16 +1,19 @@
-name = "4z0t's Score Board"
+name = "4z0t's ScoreBoard"
 version = 1
 copyright = ""
-description = [[4z0t's ScoreBoard
+description = [[4z0t's ScoreBoard:
 * Minimalistic and simple design
 * Animations
 * Flexible replay ScoreBoard
+* Coop support
 
-Requires UI Mod Tools
+Requires UI Mod Tools.
+
+See forum post for more info https://forum.faforever.com/topic/4391/new-scoreboard?_=1669035632876
 ]]
 author = "4z0t"
 url = ""
-uid = "4z0ts-score-board-v01"
+uid = "4z0ts-scoreboard-v01"
 exclusive = false
 ui_only = true
 conflicts = {}
