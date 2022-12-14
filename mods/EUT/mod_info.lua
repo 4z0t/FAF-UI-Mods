ae5ca55e8f46764b86e1f2427ecd7aae8bade276
4z0t <gastec139@gmail.com>
2022-12-14 19:44:40 +0300

    Version 9

--- mods/EUT/mod_info.lua
+++ mods/EUT/mod_info.lua
@@ -1,7 +1,11 @@
 name = "ECO UI Tools"
-version = 8
+version = 9
 copyright = ""
 description = [[Provides ECO management UI components.
+
+Overlays:
+- the one showing only upgrading mexes (UI party like)
+- the one showing tech level, capped state and upgrade progress (Eco Manager like)
 
 Options:
 - Auto t1 mex upgrade
@@ -15,7 +19,7 @@ Requires UI mod tools.
 ]]
 author = "4z0t"
 url = ""
-uid = "eco-ui-tools-4z0t-v08"
+uid = "eco-ui-tools-4z0t-v09"
 exclusive = false
 ui_only = true
 conflicts = {}
