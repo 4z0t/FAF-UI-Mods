734236d7cba1813a516989dfedbf726d4f06971c
4z0t <gastec139@gmail.com>
2022-12-30 18:11:20 +0300

    move hidden select function

--- mods/UMT/modules/select.lua
+++ mods/UMT/modules/select.lua
@@ -1,13 +1,1 @@
-local SetIgnoreSelection = import("/lua/ui/game/gamemain.lua").SetIgnoreSelection
-local CommandMode = import('/lua/ui/game/commandmode.lua')
----Performs hidden unit selection callback
----@param callback fun()
-function Hidden(callback)
-    local current_command = CommandMode.GetCommandMode()
-    local old_selection = GetSelectedUnits() or {}
-    SetIgnoreSelection(true)
-    callback()
-    SelectUnits(old_selection)
-    CommandMode.StartCommandMode(current_command[1], current_command[2])
-    SetIgnoreSelection(false)
-end
+Hidden = UMT.Units.HiddenSelect
