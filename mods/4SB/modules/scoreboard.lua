ceecd7e72c300344cf824fe9a151e7aac83fa1d0
Alexander <84857900+4z0t@users.noreply.github.com>
2023-01-01 17:03:16 +0300

    add Layout property

--- mods/4SB/modules/scoreboard.lua
+++ mods/4SB/modules/scoreboard.lua
@@ -74,6 +74,30 @@ ScoreBoard = UMT.Class(Group)
         self._mode = "income"
     end,
 
+
+
+    Layout = UMT.Property
+    {
+        set = function(self, value)
+            if self._clearLayout then
+                self:_clearLayout()
+                self._clearLayout = false
+            end
+
+            self._layout = value
+
+            if self._layout then
+                self._clearLayout = self:_layout()
+            else
+                self:_Layout()
+            end
+        end,
+        get = function(self)
+            return self._layout
+        end
+    },
+
+
     _Layout = function(self)
         if self._title then
             LayoutFor(self._title)
