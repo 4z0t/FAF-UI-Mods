efc184c2fd9c2156a37cc834ebfe41ce470e2490
Alexander <84857900+4z0t@users.noreply.github.com>
2023-01-10 15:36:26 +0300

    Move only Version 1 (#31)

---
+++ mods/Move/mod_info.lua
@@ -0,0 +1,18 @@
+name = "Move only"
+version = 1
+copyright = ""
+description = [[Adds move only order key action (like regular one), but cant be stopped until needed.
+Can be stopped with same key bind click or RMB.]]
+author = "4z0t"
+url = ""
+uid = "move-only-v01"
+icon = "/mods/Move/icon.png"
+selectable = true
+enabled = true
+exclusive = false
+ui_only = true
+requires = {}
+requiresNames = {}
+conflicts = {}
+before = {}
+after = {}
---
+++ mods/Move/modules/Main.lua
@@ -0,0 +1,46 @@
+local CM = import("/lua/ui/game/commandmode.lua")
+local KeyMapper = import('/lua/keymap/keymapper.lua')
+
+local locked = false
+
+function IsLocked()
+    return locked
+end
+
+function Reset()
+    ConExecute 'StartCommandMode order RULEUCC_Move'
+end
+
+function Toggle()
+    locked = not locked
+    Reset()
+end
+
+---comment
+---@param commandMode CommandMode
+---@param commandModeData CommandModeData
+function OnCommandStarted(commandMode, commandModeData)
+
+end
+
+---comment
+---@param commandMode CommandMode
+---@param commandModeData CommandModeData
+function OnCommandEnded(commandMode, commandModeData)
+    if locked and (commandModeData and commandModeData.name == "RULEUCC_Move") then
+        ForkThread(Reset)
+    end
+end
+
+function Main(isReplay)
+    if isReplay then return end
+
+    CM.AddStartBehavior(OnCommandStarted)
+    CM.AddEndBehavior(OnCommandEnded)
+
+end
+
+KeyMapper.SetUserKeyAction('Move only', {
+    action = 'UI_Lua import("/mods/Move/modules/Main.lua").Toggle()',
+    category = 'orders'
+})
