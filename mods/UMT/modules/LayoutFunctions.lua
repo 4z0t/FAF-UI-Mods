7cdcff2f8d5ca83f943d77de5dd78d1ce3f15009
4z0t <gastec139@gmail.com>
2023-01-15 17:15:45 +0300

    add Min function

--- mods/UMT/modules/LayoutFunctions.lua
+++ mods/UMT/modules/LayoutFunctions.lua
@@ -1,5 +1,8 @@
 local math = math
 local LayoutHelpers = import('/lua/maui/layouthelpers.lua')
+
+
+
 ---returns function of max between lazyvar or value
 ---@param var LazyVar<number>
 ---@param value number
@@ -19,11 +22,36 @@ end
 ---@return fun():number
 function Max(n1, n2)
     if iscallable(n1) and iscallable(n2) then
-        return function()
-            return math.max(n1(), n2())
-        end
+        return function() return math.max(n1(), n2()) end
     end
     if iscallable(n1) then return _MaxVarOrValue(n1, n2) end
     if iscallable(n2) then return _MaxVarOrValue(n2, n1) end
     return math.max(n1, n2)
+end
+
+
+---returns function of min between lazyvar or value
+---@param var LazyVar<number>
+---@param value number
+---@return fun():number
+local function _MinVarOrValue(var, value)
+    return function()
+        return math.min(var(), LayoutHelpers.ScaleNumber(value))
+    end
+end
+
+---returns function of min of two given lazyvars
+---@overload fun(n1:number, n2:number):number
+---@overload fun(n1:LazyVar<number>, n2:number):(fun():number)
+---@overload fun(n1:number, n2:LazyVar<number>):(fun():number)
+---@param n1 LazyVar<number>
+---@param n2 LazyVar<number>
+---@return fun():number
+function Min(n1, n2)
+    if iscallable(n1) and iscallable(n2) then
+        return function() return math.min(n1(), n2()) end
+    end
+    if iscallable(n1) then return _MinVarOrValue(n1, n2) end
+    if iscallable(n2) then return _MinVarOrValue(n2, n1) end
+    return math.min(n1, n2)
 end
