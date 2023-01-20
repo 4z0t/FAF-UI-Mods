02796a5df41cdd075481f31eeae3ff5b0c051114
Alexander <84857900+4z0t@users.noreply.github.com>
2023-01-21 02:16:53 +0300

    add scale property to Text

--- mods/UMT/modules/Views/Text.lua
+++ mods/UMT/modules/Views/Text.lua
@@ -1,8 +1,21 @@
+local LayoutHelpers = import('/lua/maui/layouthelpers.lua')
 local _Text = import("/lua/maui/text.lua").Text
 
 
 Text = UMT.Class(_Text)
 {
+
+    Scale = UMT.Property
+    {
+        get = function(self)
+            return self._scale or LayoutHelpers.GetPixelScaleFactor()
+        end,
+
+        set = function(self, value)
+            self._scale = value
+        end
+    },
+
     Text = UMT.Property
     {
         get = function(self)
@@ -32,6 +45,7 @@ Text = UMT.Class(_Text)
         end,
 
         set = function(self, value)
+            value = math.floor(value * (self._scale or LayoutHelpers.GetPixelScaleFactor()))
             self._font._pointsize:Set(value)
         end
     },
