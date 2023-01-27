0a4d89c72cb9ddd46ab3515d24f5575f21f26131
4z0t <gastec139@gmail.com>
2023-01-27 16:22:07 +0300

    upvalue ScaleNumber

--- mods/4SB/modules/Animations.lua
+++ mods/4SB/modules/Animations.lua
@@ -1,30 +1,31 @@
 local LayoutHelpers = import('/lua/maui/layouthelpers.lua')
 local LayoutFor = UMT.Layouter.ReusedLayoutFor
+local ScaleNumber = LayoutHelpers.ScaleNumber
 
 
 contractAnimation = UMT.Animation.Factory.Base
     :OnStart(function(control, state, speed, offset)
         PlaySound(Sound { Cue = "UI_Score_Window_Close", Bank = "Interface" })
-        return { speed = LayoutHelpers.ScaleNumber(speed), offset = offset }
+        return { speed = ScaleNumber(speed), offset = offset }
     end)
     :OnFrame(function(control, delta, state)
-        return control.Left() > GetFrame(0).Right() + LayoutHelpers.ScaleNumber(state.offset) or
+        return control.Left() > GetFrame(0).Right() + ScaleNumber(state.offset) or
             control.Right:Set(control.Right() + delta * state.speed)
     end)
     :OnFinish(function(control, state)
         local width = control.Width()
         LayoutFor(control)
-            :Right(function() return GetFrame(0).Right() + width + LayoutHelpers.ScaleNumber(state.offset) end)
+            :Right(function() return GetFrame(0).Right() + width + ScaleNumber(state.offset) end)
     end)
     :Create()
 
 expandAnimation = UMT.Animation.Factory.Base
     :OnStart(function(control, state, speed, offset)
         PlaySound(Sound { Cue = "UI_Score_Window_Open", Bank = "Interface" })
-        return { speed = LayoutHelpers.ScaleNumber(speed), offset = offset }
+        return { speed = ScaleNumber(speed), offset = offset }
     end)
     :OnFrame(function(control, delta, state)
-        return control.Right() < GetFrame(0).Right() - LayoutHelpers.ScaleNumber(state.offset) or
+        return control.Right() < GetFrame(0).Right() - ScaleNumber(state.offset) or
             control.Right:Set(control.Right() - delta * state.speed)
     end)
     :OnFinish(function(control, state)
