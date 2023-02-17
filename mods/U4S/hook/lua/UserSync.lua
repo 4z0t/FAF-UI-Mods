aeaf6814cb148bf6de8c61f6919f098c9008973f
4z0t <gastec139@gmail.com>
2023-02-17 17:14:36 +0300

    version 1

--- mods/U4S/hook/lua/UserSync.lua
+++ mods/U4S/hook/lua/UserSync.lua
@@ -1,8 +1,9 @@
 local _OnSync = OnSync
+local U4Sui = import("/mods/U4S/modules/UI/Main.lua")
 function OnSync()
-
-
-
-
     _OnSync()
+
+    if not Sync.UI4Sim then return end
+
+    U4Sui.Process(Sync.UI4Sim)
 end
---
+++ mods/U4S/modules/Sim/Main.lua
@@ -0,0 +1,27 @@
+---@class SimToUISyncData
+---@field name string
+---@field fileName FileName
+---@field functionName string
+---@field args table?
+---@field func fun(...:any) ?
+
+
+function ValidateArgs(args)
+
+end
+
+local callbacks = {}
+
+function Add(name, func)
+    callbacks[name] = func
+end
+
+function Remove(name)
+    callbacks[name] = nil
+end
+
+function Process(name, args, from)
+    if not callbacks[name] then return end
+
+    callbacks[name](args)
+end
