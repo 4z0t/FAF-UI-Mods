b2c23ce4bfd822859b28709f40490f4ed03f7308
4z0t <gastec139@gmail.com>
2023-02-25 22:14:13 +0300

    fix behaiviour and fix engineer assist and build

--- mods/GS/hook/lua/ui/game/commandmode.lua
+++ mods/GS/hook/lua/ui/game/commandmode.lua
@@ -1,3 +1,14 @@
 function IsIgnoredSelection()
     return ignoreSelection
 end
+
+do
+    local OldOnCommandIssued = OnCommandIssued
+    function OnCommandIssued(command)
+        OldOnCommandIssued(command)
+
+        if not command.Clear then return end
+
+        import('/mods/GS/modules/Main.lua').OnCommandIssued(commandMode, modeData, command)
+    end
+end
--- mods/GS/modules/Main.lua
+++ mods/GS/modules/Main.lua
@@ -38,11 +38,9 @@ function Next(isManual)
         end
     until not unit:IsDead()
     lastUnit = unit
-    supress = true
+
     SelectUnits { unit }
-    --TODO double next due to guard/build mode
-    --supress = not activeCommandModeData
-    --supress = unit:IsInCategory("ENGINEER")
+
     if not isManual then
         CM.StartCommandMode(activeCommandMode, activeCommandModeData)
     end
@@ -64,16 +62,17 @@ end
 ---@param commandModeData CommandModeData
 function OnCommandStarted(commandMode, commandModeData)
     if not IsActive() then return end
-    -- LOG("-------------start----------")
-    -- reprsl(commandMode)
-    -- reprsl(commandModeData)
+
+
     isStarted = true
 end
 
 ---comment
 ---@param commandMode CommandMode
 ---@param commandModeData CommandModeData
-function OnCommandEnded(commandMode, commandModeData)
+---@param command any
+function OnCommandIssued(commandMode, commandModeData, command)
+
     if not IsActive() then return end
     --if commandModeData and not commandModeData.isCancel then return end
     local selectedUnits = GetSelectedUnits()
@@ -82,14 +81,17 @@ function OnCommandEnded(commandMode, commandModeData)
         -- check if unit died for some reason
         if not lastUnit:IsDead() then Reset(false) return end
     end
-    -- LOG("-------------end----------")
-    -- reprsl(commandMode)
-    -- reprsl(commandModeData)
-    if supress then
-        supress = continuous
+
+
+    
+    if command.CommandType == 'Guard' then
+        supress = not supress
+    end
+
+    if supress or command.CommandType == 'None' or continuous then
         return
     end
-    
+
     ForkThread(Next, false)
 end
 
@@ -97,7 +99,7 @@ function Main(isReplay)
     if isReplay then return end
 
     CM.AddStartBehavior(OnCommandStarted)
-    CM.AddEndBehavior(OnCommandEnded)
+    --CM.AddEndBehavior(OnCommandEnded)
 
 end
 
