95f4f4dbf368a4649e0d94bb8628f2b279b79750
4z0t <gastec139@gmail.com>
2023-02-25 22:22:38 +0300

    GS Version 3

--- mods/GS/mod_info.lua
+++ mods/GS/mod_info.lua
@@ -1,16 +1,16 @@
 name = "Group Scatter"
-version = 2
+version = 3
 copyright = ""
 description = [[Adds two groups scatter actions that allow you to loop over units in selected group.
 
 Quick mode switches to next unit after order being given to previous one with save of current command mode (e.g. attack, launch, move)
 Continuous mode only switches to next unit if bind is pressed.
 
-!WARNING! mod is in development, there are some problems in quick mode dedicated to guard and build orders.
+Makes sound when cycle is complete. You can press to select previous selection again.
 ]]
 author = "4z0t"
 url = ""
-uid = "group-scatter-v02"
+uid = "group-scatter-v03"
 icon = "/mods/GS/icon.png"
 selectable = true
 enabled = true
