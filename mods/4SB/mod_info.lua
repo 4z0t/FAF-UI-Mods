5c88a3cb82a17acef712a1ca9c165fbf4c07c425
Alexander <84857900+4z0t@users.noreply.github.com>
2023-03-01 12:25:52 +0300

    4SB Version 7 (#44)
    * fix preview in replays
    
    * add option for displaying of army color

--- mods/4SB/mod_info.lua
+++ mods/4SB/mod_info.lua
@@ -1,5 +1,5 @@
 name = "4z0t's ScoreBoard"
-version = 6
+version = 7
 copyright = ""
 description = [[4z0t's ScoreBoard:
 * Minimalistic and simple design
@@ -14,7 +14,7 @@ See forum post for more info https://forum.faforever.com/topic/4391/new-scoreboa
 author = "4z0t"
 url = ""
 icon = "/mods/4SB/icon.png"
-uid = "4z0ts-scoreboard-v06"
+uid = "4z0ts-scoreboard-v07"
 exclusive = false
 ui_only = true
 conflicts = {}
