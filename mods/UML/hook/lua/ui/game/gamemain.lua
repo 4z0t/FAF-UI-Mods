75954d2f5f5172ce09c2589723c51ed53fd6996b
Alexander <84857900+4z0t@users.noreply.github.com>
2023-04-03 13:57:33 +0300

    UML Version 1 (#49)

---
+++ mods/UML/hook/lua/ui/game/gamemain.lua
@@ -0,0 +1,9 @@
+do
+    _ = __active_mods
+        | UMT.LuaQ.where(function(v) return v.__umt end)
+        | UMT.LuaQ.select(function(v) return string.sub(v.location, 7) --[[cut "/mods/"]] end)
+        | UMT.LuaQ.foreach(function(_, folderName)
+            UMT.Mods.Add(folderName)
+            LOG("UML: added " .. folderName)
+        end)
+end
---
+++ mods/UML/mod_info.lua
@@ -0,0 +1,18 @@
+name = "UI mods loader"
+uid = "ui-mod-loader-v01"
+version = 1
+copyright = ""
+description = [[]]
+author = "4z0t"
+url = ""
+selectable = true
+enabled = true
+exclusive = false
+ui_only = true
+requiresNames = {}
+conflicts = {}
+after = {}
+icon = "/mods/UML/icon.png"
+requires = {
+    "ui-mod-tools-4z0t-v09"
+}
