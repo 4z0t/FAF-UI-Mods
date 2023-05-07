d20003ab9f6cdc26715883313d9b56b2be4834fe
4z0t <gastec139@gmail.com>
2023-05-07 21:19:50 +0300

    init

---
+++ mods/ContextTemplates/mod_info.lua
@@ -0,0 +1,12 @@
+name = "Context Templates"
+version = 1
+copyright = ""
+description = [[
+Requires UI Mod Tools V8
+]]
+author = "4z0t"
+url = ""
+uid = "context-templates-4z0t-v01"
+exclusive = false
+ui_only = true
+conflicts = {}
