3ca8f7722d21f4598330f7978627fa58f024461f
Alexander <84857900+4z0t@users.noreply.github.com>
2023-09-22 15:36:01 +0300

    move Mult up

--- mods/UMT/modules/LayoutFunctions.lua
+++ mods/UMT/modules/LayoutFunctions.lua
@@ -11,6 +11,31 @@ local defaultScaleFactor = LayoutHelpers.GetPixelScaleFactor()
 ---@alias NumberFunction (fun():number) | NumberVar
 
 
+---returns function of mult of lazyvar and value
+---@param var NumberVar
+---@param value number
+---@return NumberFunction
+local function _MultVarAndValue(var, value)
+    if value == 0 then return 0 end
+    return function() return var() * value end
+end
+
+---returns function of mult of two given lazyvars
+---@overload fun(n1:number, n2:number):number
+---@overload fun(n1:NumberVar, n2:number):NumberFunction
+---@overload fun(n1:number, n2:NumberVar):NumberFunction
+---@param n1 NumberVar
+---@param n2 NumberVar
+---@return NumberFunction
+function Mult(n1, n2)
+    if iscallable(n1) and iscallable(n2) then
+        return function() return n1() * n2() end
+    end
+    if iscallable(n1) then return _MultVarAndValue(n1, n2) end
+    if iscallable(n2) then return _MultVarAndValue(n2, n1) end
+    return n1 * n2
+end
+
 ---returns function of max between lazyvar or value
 ---@param var NumberVar
 ---@param value number
@@ -170,30 +195,7 @@ function Sum(n1, n2, scale)
     return (n1 + n2) * scale
 end
 
----returns function of mult of lazyvar and value
----@param var NumberVar
----@param value number
----@return NumberFunction
-local function _MultVarAndValue(var, value)
-    if value == 0 then return 0 end
-    return function() return var() * value end
-end
 
----returns function of mult of two given lazyvars
----@overload fun(n1:number, n2:number):number
----@overload fun(n1:NumberVar, n2:number):NumberFunction
----@overload fun(n1:number, n2:NumberVar):NumberFunction
----@param n1 NumberVar
----@param n2 NumberVar
----@return NumberFunction
-function Mult(n1, n2)
-    if iscallable(n1) and iscallable(n2) then
-        return function() return n1() * n2() end
-    end
-    if iscallable(n1) then return _MultVarAndValue(n1, n2) end
-    if iscallable(n2) then return _MultVarAndValue(n2, n1) end
-    return n1 * n2
-end
 
 ---returns function of floor of a given lazyvar
 ---@overload fun(n:number):number
