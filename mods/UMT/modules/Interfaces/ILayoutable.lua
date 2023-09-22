7e141c0250161892af00d4528284c4c6052cec08
4z0t <gastec139@gmail.com>
2023-09-22 13:44:23 +0300

    annotate ILayoutable

--- mods/UMT/modules/Interfaces/ILayoutable.lua
+++ mods/UMT/modules/Interfaces/ILayoutable.lua
@@ -1,11 +1,16 @@
+---@class ILayoutable
+---@field _clearLayout fun(control: ILayoutable)?
+---@field _layout fun(control: ILayoutable) : fun(control: ILayoutable)?
 ILayoutable = ClassSimple
 {
     Layout = UMT.Property
     {
+        ---@param self ILayoutable
+        ---@param value any
         set = function(self, value)
             if self._clearLayout then
                 self:_clearLayout()
-                self._clearLayout = false
+                self._clearLayout = nil
             end
 
             self._layout = value
@@ -16,12 +21,14 @@ ILayoutable = ClassSimple
                 self:_Layout()
             end
         end,
+        ---@param self ILayoutable
+        ---@return fun(control: ILayoutable)
         get = function(self)
             return self._layout or self._Layout
         end
     },
 
-
+    ---@param self ILayoutable
     _Layout = function(self)
         error "Not implemented _Layout method!"
     end
