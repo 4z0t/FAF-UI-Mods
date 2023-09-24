b9e39d862c796e3b01e4dd8ba4bcbea32ee8f40a
4z0t <gastec139@gmail.com>
2023-09-24 20:52:54 +0300

    wip

--- mods/4SB/modules/ObserverPanel.lua
+++ mods/4SB/modules/ObserverPanel.lua
@@ -20,12 +20,15 @@ local sliderColor = Options.observer.color.line:Raw()
 local width = 300
 local height = 20
 
----@class ObserverPanel : Group
+---@class ObserverPanel : Group, ILayoutable
 ---@field _bg Bitmap
-ObserverPanel = Class(Group)
+ObserverPanel = Class(Group, UMT.Interfaces.ILayoutable)
 {
+    ---@param self ObserverPanel
+    ---@param parent Control
     __init = function(self, parent)
         Group.__init(self, parent)
+        self:InitLayouter(parent)
 
         self._bg = Bitmap(self)
 
@@ -40,8 +43,6 @@ ObserverPanel = Class(Group)
         self._slider.OnValueSet = function(slider, newValue)
             ConExecute("WLD_GameSpeed " .. tostring(newValue))
         end
-
-
 
         self._speed = Text(self)
         self._speed:SetFont(obsTextFont, obsTextSize)
@@ -58,37 +59,39 @@ ObserverPanel = Class(Group)
     end,
 
     __post_init = function(self)
-        self:_Layout()
+        self:Layout()
     end,
 
-    _Layout = function(self)
+    ---@param self ObserverPanel
+    ---@param layouter UMT.Layouter
+    _Layout = function(self, layouter)
 
-        LayoutFor(self._bg)
+        layouter(self._bg)
             :Fill(self)
             :Color(bgColor)
             :DisableHitTest()
 
 
-        LayoutFor(self._observerText)
+        layouter(self._observerText)
             :AtLeftTopIn(self, 10, 2)
             :Over(self, 10)
             :DisableHitTest()
 
-        LayoutFor(self._slider)
+        layouter(self._slider)
             :AtVerticalCenterIn(self)
             :AtRightIn(self, 25)
             :RightOf(self._observerText, 5)
             :Height(height - 4)
             :Over(self, 10)
 
-        LayoutFor(self._speed)
+        layouter(self._speed)
             :AtVerticalCenterIn(self)
             :RightOf(self._slider, 5)
             :Over(self, 10)
             :DisableHitTest()
 
-        LayoutFor(self)
-            :Width(UMT.Layouter.Functions.Min(self:GetParent().Width, width))
+        layouter(self)
+            :Width(layouter:Min(self:GetParent().Width, width))
             :Height(height)
 
     end,
