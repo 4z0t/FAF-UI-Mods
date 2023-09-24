7277a03d1dc9bd9144eec2e523b7483ddca3eb5e
4z0t <gastec139@gmail.com>
2023-09-24 18:44:24 +0300

    cleanup

--- mods/UMT/modules/OptionsWindow.lua
+++ mods/UMT/modules/OptionsWindow.lua
@@ -150,14 +150,12 @@ OptionsWindow = Class(Window) {
             :Height(10)
             :ResetBottom()
             :Over(self.ClientGroup)
-            :End()
 
         local okBtn = UIUtil.CreateButtonStd(self, '/widgets02/small', '<LOC _Ok>', 16)
         LayoutFor(okBtn)
             :Below(self._optionsGroup, 4)
             :AtLeftIn(self._optionsGroup)
             :Over(self._optionsGroup)
-            :End()
 
         okBtn.OnClick = function(control)
             self:OnClose(true)
@@ -173,7 +171,6 @@ OptionsWindow = Class(Window) {
             :AtRightIn(self._optionsGroup)
             :ResetLeft()
             :Over(self._optionsGroup)
-            :End()
         LayoutFor(self)
             :ResetRight()
             :Width(function()
