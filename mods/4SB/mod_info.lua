858ff4b803ecd2d7279e130b883b8e451e0aacec
4z0t <gastec139@gmail.com>
2023-12-21 19:47:16 +0300

    4SB Version 12

--- mods/4SB/mod_info.lua
+++ mods/4SB/mod_info.lua
@@ -1,5 +1,5 @@
 name = "4z0t's ScoreBoard"
-version = 11
+version = 12
 copyright = ""
 description = [[4z0t's ScoreBoard:
 * Minimalistic and simple design
@@ -14,7 +14,7 @@ See forum post for more info https://forum.faforever.com/topic/4391/new-scoreboa
 author = "4z0t"
 url = ""
 icon = "/mods/4SB/icon.png"
-uid = "4z0ts-scoreboard-v11"
+uid = "4z0ts-scoreboard-v12"
 exclusive = false
 ui_only = true
 conflicts = {}
