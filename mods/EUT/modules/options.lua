ff80f914178ca1fccb925b5bb6e776d04b1c5b9d
4z0t <gastec139@gmail.com>
2023-12-25 17:07:37 +0300

    remove do..end

--- mods/EUT/modules/options.lua
+++ mods/EUT/modules/options.lua
@@ -1,39 +1,34 @@
-do
-    local Options = UMT.Options
-    local OptionVar = UMT.OptionVar.Create
+local Options = UMT.Options
+local OptionVar = UMT.OptionVar.Create
 
-    local modName = "EUT"
-    local function EUTOption(name, default)
-        return OptionVar(modName, name, default)
-    end
+local modName = "EUT"
 
-    local Opt = UMT.Options.Opt
+local Opt = UMT.Options.Opt
 
-    UMT.Options.Mods["EUT"] = {
-        overlayOption = Opt(true),
-        useNumberOverlay = Opt(false),
-        overlaySize = Opt(10),
-        upgradeT1Option = Opt(false),
-        upgradeT2Option = Opt(false),
-        unpauseAssisted = Opt(false),
-        unpauseOnce = Opt(false),
-        unpauseAssistedBP = Opt(4),
-        panelScale = Opt(100),
-    }
+UMT.Options.Mods["EUT"] = {
+    overlayOption = Opt(true),
+    useNumberOverlay = Opt(false),
+    overlaySize = Opt(10),
+    upgradeT1Option = Opt(false),
+    upgradeT2Option = Opt(false),
+    unpauseAssisted = Opt(false),
+    unpauseOnce = Opt(false),
+    unpauseAssistedBP = Opt(4),
+    panelScale = Opt(100),
+}
 
-    function Main()
-        local options = UMT.Options.Mods["EUT"]
-        Options.AddOptions(modName, "ECO UI Tools", {
-            Options.Filter("Show mex overlay", options.overlayOption),
-            Options.Filter("Auto T1 mex upgrade", options.upgradeT1Option),
-            Options.Filter("Auto T2 capped mex upgrade", options.upgradeT2Option),
-            Options.Filter("Display mex overlay with numbers", options.useNumberOverlay),
-            Options.Slider("Number overlay size", 5, 25, 1, options.overlaySize),
-            Options.Filter("Unpause mexes being assisted", options.unpauseAssisted),
-            Options.Filter("Unpause once", options.unpauseOnce),
-            Options.Slider("Assist BP threshold: mex unpauses if summary BP greater than this value", 4, 100, 1,
-                options.unpauseAssistedBP),
-            Options.Slider("Panel scale, %", 50, 300, 10, options.panelScale)
-        })
-    end
+function Main()
+    local options = UMT.Options.Mods["EUT"]
+    Options.AddOptions(modName, "ECO UI Tools", {
+        Options.Filter("Show mex overlay", options.overlayOption),
+        Options.Filter("Auto T1 mex upgrade", options.upgradeT1Option),
+        Options.Filter("Auto T2 capped mex upgrade", options.upgradeT2Option),
+        Options.Filter("Display mex overlay with numbers", options.useNumberOverlay),
+        Options.Slider("Number overlay size", 5, 25, 1, options.overlaySize),
+        Options.Filter("Unpause mexes being assisted", options.unpauseAssisted),
+        Options.Filter("Unpause once", options.unpauseOnce),
+        Options.Slider("Assist BP threshold: mex unpauses if summary BP greater than this value", 4, 100, 1,
+            options.unpauseAssistedBP),
+        Options.Slider("Panel scale, %", 50, 300, 10, options.panelScale)
+    })
 end
