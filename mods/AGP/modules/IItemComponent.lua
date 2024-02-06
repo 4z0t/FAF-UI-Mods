aba0aeab3a0d01b3db767c2c2d84dce08f4b6f61
4z0t <gastec139@gmail.com>
2024-02-06 20:13:09 +0300

    cleanup

--- mods/AGP/modules/IItemComponent.lua
+++ mods/AGP/modules/IItemComponent.lua
@@ -8,8 +8,7 @@ IItemComponent = Class()
     ---Called when component is bond to an item
     ---@param self IItemComponent
     ---@param item Item
-    Create = function (self, item)
-        
+    Create = function(self, item)
     end,
 
     ---Called when grid item receives an event
@@ -17,32 +16,27 @@ IItemComponent = Class()
     ---@param item Item
     ---@param event KeyEvent
     HandleEvent = function(self, item, event)
-        
     end,
-    
+
     ---Called when item is activated with this component event handling
     ---@param self IItemComponent
     ---@param item Item
     Enable = function(self, item)
-        
     end,
-    
+
     ---@param self IItemComponent
     ---@param action string
-    SetAction = function (self, action)
-        
+    SetAction = function(self, action)
     end,
-    
+
     ---Called when item is changing event handler
     ---@param self IItemComponent
     ---@param item Item
     Disable = function(self, item)
-
     end,
 
     ---Called when component is being destroyed
     ---@param self IItemComponent
     Destroy = function(self)
-
     end,
 }
