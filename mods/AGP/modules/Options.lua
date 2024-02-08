3f73c52e9f6806013c8c0b0bd3cb7bcca9d56547
4z0t <gastec139@gmail.com>
2024-02-08 14:36:04 +0300

    cleanup

--- mods/AGP/modules/Options.lua
+++ mods/AGP/modules/Options.lua
@@ -4,8 +4,6 @@ local Opt = UMT.Options.Opt
 UMT.Options.Mods["AGP"] = {
     rows = Opt(4),
     columns = Opt(4),
-    itemWidth = Opt(32),
-    itemHeight = Opt(32),
     itemSize = Opt(32),
     space = Opt(3),
 }
@@ -17,7 +15,6 @@ function Main()
         Options.Slider("Columns", 1, 10, 1, options.columns, 4),
         Options.Slider("Size", 10, 64, 1, options.itemSize, 4),
         Options.Slider("Space", 0, 10, 1, options.space, 4),
-        --Options.Slider("Height", 10, 64, 1, options.itemHeight, 4),
     })
 
     Options.AddOptions("AGP_Ext", "Actions Grid Extensions", function(parent)
