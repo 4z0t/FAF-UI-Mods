535656de0dd4dcbe6efa1a11249cc1bbcdcb761e
4z0t <gastec139@gmail.com>
2024-03-20 17:10:22 +0300

    PAAAAAAIIIIIIIN

--- mods/Ctrl/hook/lua/ui/controls/worldview.lua
+++ mods/Ctrl/hook/lua/ui/controls/worldview.lua
@@ -41,14 +41,28 @@ do
     local _WorldViewHandleEvent = WorldView.HandleEvent
     WorldView = Class(WorldView) {
         ReturnHitTest = false,
+        WasCopying = false,
         ---@param self WorldView
         ---@param event KeyEvent
         HandleEvent = function(self, event)
+            -- return our hit test back, since we dont wanna lose it,
+            -- worldview is our primary way of controling units
             if self.ReturnHitTest then
+                self.ReturnHitTest = false
                 self:EnableHitTest()
             end
+            -- this one is called very inconsistenly.
+            -- when we hold and drag it is called in the end.
+            -- but when we just press it, it is not called at all, so we have no idea wheter we clicked
+            -- or dragged, because if we press during command mode again it is called xdddddd
+            if event.Type == "ButtonRelease" and self.WasCopying then
+                self.WasCopying = false
+                return event.Modifiers.Ctrl
+            end
+            -- Check our primary stuff here.
             if event.Type == "ButtonPress" and event.Modifiers.Ctrl and event.Modifiers.Right then
                 if CopyBuilding() then
+                    self.WasCopying = true
                     self.ReturnHitTest = true
                     self:DisableHitTest()
                 end
