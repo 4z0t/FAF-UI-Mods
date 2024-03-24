8f3e3ceaa8974167eaaeca43e9105803d0e97995
4z0t <gastec139@gmail.com>
2024-03-24 18:40:55 +0300

    Ctrl Version 2

--- mods/Ctrl/mod_info.lua
+++ mods/Ctrl/mod_info.lua
@@ -1,14 +1,17 @@
 name = "Ctrl"
-version = 1
+version = 2
 copyright = ""
 description = [[
 Extends application of Ctrl button:
     - Ctrl-Move locks move order while holding ctrl
     - Ctrl-Copy hold Ctrl and right click any building to copy it for building
+
+You can enable/disable features any time in UI mods options menu.
+Requires UI Mod Tools!
 ]]
 author = "4z0t"
 url = ""
-uid = "ctrl-v01"
+uid = "ctrl-v02"
 icon = "/mods/Ctrl/icon.png"
 selectable = true
 enabled = true
