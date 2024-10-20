c92b1740027370b26dd887e5faee1ef5d70d89ec
4z0t <gastec139@gmail.com>
2024-10-21 00:23:34 +0300

    doesnt work

--- mods/GuessELO/hook/lua/ui/game/gamemain.lua
+++ mods/GuessELO/hook/lua/ui/game/gamemain.lua
@@ -1,6 +1,33 @@
 do
     local _GetArmiesTable = _G.GetArmiesTable
+
+    local function ShuffleColors()
+        local armiesTable = _GetArmiesTable()
+        local colors = {}
+        for i, client in ipairs(armiesTable.armiesTable) do
+            if (not client.civilian) then
+                client.nickname = string.format('Player %d', i)
+                table.insert(colors, client.color)
+            end
+        end
+        colors = table.shuffle(colors)
+        TeamColorMode(table.concat(colors, ","))
+        TeamColorMode(true)
+        for i, client in ipairs(armiesTable.armiesTable) do
+            if (not client.civilian) then
+                client.color = colors[i]
+            end
+        end
+        return armiesTable
+    end
+
+    local once = true
+
     _G.GetArmiesTable = function()
+        -- if once then
+        --     once = false
+        --     return ShuffleColors()
+        -- end
         local armiesTable = _GetArmiesTable()
         for i, client in ipairs(armiesTable.armiesTable) do
             if (not client.civilian) then
@@ -9,7 +36,6 @@ do
         end
         return armiesTable
     end
-
 
     local _GetRolloverInfo = _G.GetRolloverInfo
     _G.GetRolloverInfo = function()
@@ -20,9 +46,6 @@ do
         return _info
     end
 
-end
-
-do
     local _CreateUI = CreateUI
     function CreateUI(isReplay)
         _CreateUI(isReplay)
