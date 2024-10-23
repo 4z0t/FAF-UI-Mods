f51c65460faf215a73a701939eccc8b6e45adaa8
4z0t <gastec139@gmail.com>
2024-10-24 01:35:33 +0300

    remove messages with names

--- mods/GuessELO/hook/lua/ui/game/chat.lua
+++ mods/GuessELO/hook/lua/ui/game/chat.lua
@@ -7,6 +7,19 @@ do
         local changedName = armiesTable.armiesTable[i].nickname
         local i = table.find(originalNames, msg.from)
         msg.from = armiesTable.armiesTable[i].nickname
+
+
+        local found = false
+        for _, name in originalNames do
+
+            if string.find(msg.text:lower(), name:lower()) then
+                found = true
+                break
+            end
+        end
+        if found then
+            return
+        end
         return _ReceiveChatFromSim(changedName, msg)
     end
 end
