c98d3701595df33845e8ab68b4bf80755d321e00
4z0t <gastec139@gmail.com>
2024-11-19 20:27:22 +0300

    add Rings For All
    init RFA
    
    add options
    
    Update mod_info.lua
    
    add icon
    
    show alt range for factories
    
    add antinavy
    
    Update options.lua
    
    init option
    
    update to version 2
    
    update mod

---
+++ mods/RFA/mod_info.lua
@@ -0,0 +1,22 @@
+name = "Rings For All"
+uid = "rings-for-all-v03"
+version = 3
+copyright = ""
+description = [[
+Shows radiuses of weapons of selected units and build range. Also shows them with unit under mouse.
+See options for mod in UI mods options menu!
+
+Requires UI mod tools!
+]]
+author = "4z0t"
+icon = "/mods/RFA/icon.png"
+url = ""
+selectable = true
+enabled = true
+exclusive = false
+ui_only = true
+requires = {}
+requiresNames = {}
+conflicts = {}
+before = {}
+after = {}
