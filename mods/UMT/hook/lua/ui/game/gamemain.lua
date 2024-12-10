c5fbb71e479c770c04f808ed570e263de17ba6c9
4z0t <gastec139@gmail.com>
2024-12-10 21:53:25 +0300

    use in gamemain.lua

--- mods/UMT/hook/lua/ui/game/gamemain.lua
+++ mods/UMT/hook/lua/ui/game/gamemain.lua
@@ -1,11 +1,13 @@
 do
-    _ = __active_mods
-        | UMT.LuaQ.where(function(v) return v.__umt and v.ui_only end)
-        | UMT.LuaQ.select(function(v) return string.sub(v.location, 7) --[[cut "/mods/"]] end)
-        | UMT.LuaQ.foreach(function(_, folderName)
+    local Fun = UMT.Functional.Functors
+    local addUMTMods = Fun.pairs
+        | Fun.where(function(v) return v.__umt and v.ui_only end)
+        | Fun.select(function(v) return string.sub(v.location, 7) --[[cut "/mods/"]] end)
+        | Fun.foreach(function(_, folderName)
             UMT.Mods.Add(folderName)
             LOG("UML: added " .. folderName)
         end)
+        | Fun.applyTo(__active_mods)
 
     local _CreateUI = CreateUI
     function CreateUI(isReplay)
