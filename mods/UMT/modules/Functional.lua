79c34fd859ea1d65f490c8cf03df43293da4b4bb
4z0t <gastec139@gmail.com>
2024-12-14 21:37:28 +0300

    cleanup

--- mods/UMT/modules/Functional.lua
+++ mods/UMT/modules/Functional.lua
@@ -4,97 +4,6 @@ local TableInsert = table.insert
 local getmetatable = getmetatable
 local next = next
 
--- ---@generic R
--- ---@generic K
--- ---@generic V?
--- ---@class FunctionalTransformer<R, K, V>
--- ---@field private fn function
--- local FunctionalTransformer = {
---     ---@param self FunctionalTransformer
---     ---@param func function
---     ---@return FunctionalTransformer
---     __call = function(self, func)
---         self.fn = func
---         return self
---     end
--- }
-
--- ---Pops function from functional transformer
--- ---@param fnObj FunctionalTransformer
--- ---@return function
--- local function PopFn(fnObj)
---     local fn = fnObj.fn
---     fnObj.fn = nil
---     return fn
--- end
-
--- ---@class Comparator
--- ---@field fn fun(a, b):boolean
-
--- ---@class Selector
--- ---@field fn fun(v:any):any
-
--- ---@class SelectorKV
--- ---@field fn fun(k, v):any
-
--- ---@class Conditional
--- ---@field fn fun(v):boolean
-
--- ---@class ConditionalKV
--- ---@field fn fun(k, v):boolean
-
--- ---@alias Yielder fun(t:table, k):(any,any)
-
--- ---@class BORPipe<K,V> : {iterator:fun(iterator:fun(t:table<K,V>, k:K):(K,V), self:FunctionalTransformer):(fun(t:table<K,V>, k:K):(K,V))}
-
--- ---@class Iterator
--- ---@field fn fun(iterator, key):(any,any)
--- ---@operator call(table):Yielder,table
-
--- Iterator = {
---     __call = function(self, t)
---         return self.fn(t)
---     end,
-
---     __bor = function(l, r)
-
---         local borl = getmetatable(l).__bor
---         local borr = getmetatable(r).__bor
---         if borl and borr then -- we are on left side called by right one
---             return borr(l, r)
---         end
-
---         if borr then
---             return r.fn(l)
---         end
-
---         error "Unexpected order of Stateless iterator"
---     end,
--- }
-
--- ---@param fn fun(iterator, key):(any,any)
--- ---@return Iterator
--- local function CreateGenerator(fn)
---     return setmetatable({ fn = fn }, Iterator)
--- end
-
--- BORPipe = table.combine(FunctionalTransformer, {
---     ---@generic K,V
---     ---@param proto Iterator
---     ---@param self BORPipe<K,V>
---     __bor = function(proto, self)
---         return CreateGenerator(self.iterator(proto.fn, self))
---     end
--- })
-
--- ---@generic K
--- ---@generic V
--- ---@generic Class: fa-class
--- ---@return Class
--- local function MakePipe()
---     return BORPipe
--- end
-
 ---@generic K, V
 ---@param t table<K, V>
 ---@param k? K
@@ -124,79 +33,6 @@ end
 --     return k, v
 -- end)
 
-
--- ---@class FunctionalPipe
--- ---@operator bor(fun(t:table, k):(any,any)):fun(t:table, k):(any,any)
--- ---@operator call(fun():any):FunctionalPipe
-
-
--- ---@generic K
--- ---@generic V
--- ---@param bor fun(iterator:fun(t:table<K,V>, k:K):(K,V), self:FunctionalTransformer):(fun(t:table<K,V>, k:K):(K,V))
--- ---@return FunctionalPipe
--- local function MakeFunctionalPipe(bor)
---     return setmetatable({ iterator = bor }, MakePipe())
--- end
-
--- where = MakeFunctionalPipe(function(iterator, self)
---     local selector = PopFn(self)
---     return function(t, k)
---         local nk = k
---         local v
---         repeat
---             nk, v = iterator(t, nk)
---             if nk == nil then
---                 return nil, nil
---             end
---         until selector(v)
---         return nk, v
---     end
--- end)
-
-
--- select = MakeFunctionalPipe(function(iterator, self)
---     local selector = PopFn(self)
-
---     if iscallable(selector) then
---         return function(t, k)
---             local nk, v = iterator(t, k)
---             if nk == nil then return nil, nil end
---             return nk, selector(v)
---         end
---     elseif type(selector) == "string" then
---         return function(t, k)
---             local nk, v = iterator(t, k)
---             if nk == nil then return nil, nil end
---             return nk, v[selector]
---         end
---     end
-
---     error("Unsupported selector type " .. tostring(selector))
--- end)
-
-
--- foreach = MakeFunctionalPipe(function(iterator, self)
---     local func = PopFn(self)
---     return function(t, k)
---         local nk, v = iterator(t, k)
---         if nk == nil then return nil, nil end
-
---         func(nk, v)
-
---         return nk, v
---     end
--- end)
-
--- toArray = MakeFunctionalPipe(function(iterator, self)
---     return function(t)
---         local nt = {}
---         for _, v in iterator, t do
---             TableInsert(nt, v)
---         end
---         return nt
---     end
--- end)
-
 -- toTable = MakeFunctionalPipe(function(iterator, self)
 --     return function(t)
 --         local nt = {}
@@ -210,25 +46,6 @@ end
 -- toIterator = MakeFunctionalPipe(function(iterator, self)
 --     return function(t)
 --         return iterator, t
---     end
--- end)
-
--- keys = MakeFunctionalPipe(function(iterator, self)
---     return function(t, k)
---         local nk, v = iterator(t, k)
---         if nk == nil then return nil, nil end
-
---         return nk, nk
---     end
--- end)
-
--- toSet = MakeFunctionalPipe(function(iterator, self)
---     return function(t)
---         local nt = {}
---         for k, v in iterator, t do
---             nt[v] = true
---         end
---         return nt
 --     end
 -- end)
 
@@ -829,5 +646,24 @@ Functors = {
         return function(t)
             return iterator, t
         end
-    end)
+    end),
+
+    toArray = FEE(function(self, iterator, transformer)
+        if transformer then
+            return function(t)
+                local nt = {}
+                for k, v in iterator, transformer(t) do
+                    TableInsert(nt, v)
+                end
+                return nt
+            end
+        end
+        return function(t)
+            local nt = {}
+            for k, v in iterator, t do
+                TableInsert(nt, v)
+            end
+            return nt
+        end
+    end),
 }
