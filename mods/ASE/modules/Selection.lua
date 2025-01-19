8a1a3c3e76381ef4adde0d7e9762c440b6d79bef
4z0t <gastec139@gmail.com>
2025-01-19 14:54:31 +0300

    remove that feature

--- mods/ASE/modules/Selection.lua
+++ mods/ASE/modules/Selection.lua
@@ -228,7 +228,7 @@ function SetDomain(i)
     currentDomainOrderOption:Set(i)
     currentDomainOrderOption:Save()
 
-    UpdateDomainsCursor()
+    -- UpdateDomainsCursor()
 end
 
 function RotateDomains()
