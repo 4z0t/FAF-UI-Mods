3f97ed823f96e1b9aac2f384407f2f5bc53ed4fc
Alexander <gastec139@gmail.com>
2025-11-14 11:48:40 +0300

    update version

--- mods/ACUEnhancements/mod_info.lua
+++ mods/ACUEnhancements/mod_info.lua
@@ -1,13 +1,13 @@
 name = "ACU Enhancements"
-version = 1
+version = 2
 copyright = ""
 description = [[
 ]]
 author = "4z0t"
 url = ""
-uid = "acu-enhancements-v01"
+uid = "acu-enhancements-v1.0.1"
 exclusive = false
 ui_only = true
 conflicts = {}
 
-ReUI = 'ACUEnhancements=1.0.0'
+ReUI = 'ACUEnhancements=1.0.1'
