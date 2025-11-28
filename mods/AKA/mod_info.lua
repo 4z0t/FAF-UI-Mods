e03a0ef76644d85f847d55c2eff099e409e1e3b4
Alexander <gastec139@gmail.com>
2025-11-28 13:55:33 +0300

    update version

--- mods/AKA/mod_info.lua
+++ mods/AKA/mod_info.lua
@@ -1,21 +1,23 @@
 name = "Advanced Key Actions"
-version = 1
+version = 2
 copyright = "MIT License"
 description = [[
 Adds these keybinds that combine multiple actions into one based on current selection:
 * Transportation / Overcharge / Repeat queue
 * Launch missile / attack-reclaim / attack order
 * Select nearest idle t1 engineer / reclaim / toggle shields / toggle stealth
-* Move / Select nearest transport
+* Move / Select nearest idle transport
 * Select nearest air scout / build sensors
+* Zoom out / Soft stop / Hard stop
+* Zoom out / Hard stop
 
 Can be found at ReUI.Actions section.
 ]]
 author = "4z0t"
 url = "https://github.com/4z0t/FAF-UI-Mods"
-uid = "advanced-key-actions-v01"
+uid = "advanced-key-actions-1.1.0"
 exclusive = false
 ui_only = true
 conflicts = {}
 
-ReUI = "AKA=1.0.0"
+ReUI = "AKA=1.1.0"
