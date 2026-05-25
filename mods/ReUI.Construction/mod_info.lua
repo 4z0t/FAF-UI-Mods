1e5a11f38d64218c20217c0aba7c1a87ae1968bc
Alexander <gastec139@gmail.com>
2026-05-25 20:40:40 +0300

    update version

--- mods/ReUI.Construction/mod_info.lua
+++ mods/ReUI.Construction/mod_info.lua
@@ -1,6 +1,6 @@
 name = "ReUI.Construction"
-uid = "ReUI.Construction-1.2.0"
-version = 5
+uid = "ReUI.Construction-1.2.1"
+version = 6
 copyright = ""
 description = [[Check on Github and official Discord server for mode details.
 https://github.com/4z0t/FAF-UI-Mods
@@ -14,4 +14,4 @@ enabled = true
 exclusive = false
 ui_only = true
 
-ReUI = 'ReUI.Construction=1.2.0'
+ReUI = 'ReUI.Construction=1.2.1'
