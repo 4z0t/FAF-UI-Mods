c243d54ad82440ff3101675079a398da1705b1fa
Alexander <gastec139@gmail.com>
2026-06-05 11:53:01 +0300

    update on focus army changed

--- mods/ReUI.Score/mod_info.lua
+++ mods/ReUI.Score/mod_info.lua
@@ -2,12 +2,11 @@ name = "ReUI.Score"
 uid = "reui-score-1.2.4"
 version = 5
 copyright = ""
-description = [[Thanks HotCheese for adding Union Control Support
-Check on Github and official Discord server for more details.
+description = [[Check on Github and official Discord server for more details.
 https://github.com/4z0t/FAF-UI-Mods
 https://discord.gg/EZb6h6gbWz
 ]]
-author = "4z0t"
+author = "4z0t, HotCheese"
 icon = "/mods/ReUI.Score/icon.png"
 url = ""
 selectable = true
